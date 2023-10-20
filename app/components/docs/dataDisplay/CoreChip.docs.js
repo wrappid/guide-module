c75d87b8d729e80d7695ed5ee607fc4395368e46
Pritam Mondal <pritammondal8981@gmail.com>
2023-10-20 17:46:23 +0530

    + add CoreChip

--- app/components/docs/dataDisplay/CoreChip.docs.js
+++ app/components/docs/dataDisplay/CoreChip.docs.js
@@ -11,7 +11,10 @@ export default function CoreChipDocs() {
     <>
       <CoreH4>CoreChip</CoreH4>
       <CoreTypographyBody1>
-        COMPONENT_DESCRIPTION
+      Chips are compact elements that represent an input, attribute, or action.
+      Chips allow users to enter information, make selections, filter content, or trigger actions.
+
+While included here as a standalone component, the most common use will be in some form of input, so some of the behavior demonstrated here is not shown in context.
         </CoreTypographyBody1>
       <CodeSample
         title={"TITLE_OF_THE_SAMPLE"}
@@ -19,6 +22,24 @@ export default function CoreChipDocs() {
         code={`PRE-FORMATTED_CODE_GOES_HERE`}
         renderElement={<></>}
         />
+
+      <CodeSample
+        title={"Basic chip"}
+        description={"The Chip component supports outlined and filled styling."}
+        code={`<CoreChip label="Chip Filled" />
+<CoreChip label="Chip Outlined" variant="outlined" />`}
+        renderElement={<>
+          <CoreChip label="Chip Filled" />
+          <CoreChip label="Chip Outlined" variant="outlined" />        
+        </>}
+        />
+
+      {/* <CodeSample
+        title={"TITLE_OF_THE_SAMPLE"}
+        description={"DESCRIPTION_OF_THE_SAMPLE"}
+        code={`PRE-FORMATTED_CODE_GOES_HERE`}
+        renderElement={<></>}
+        />   */}
       <ComponentProps component={CoreChip} />
     </>
   );
