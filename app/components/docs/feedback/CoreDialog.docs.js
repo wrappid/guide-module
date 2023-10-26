e089f9cb8295a9779a0100b3025bba395d2e908a
Pritam Mondal <pritammondal8981@gmail.com>
2023-10-26 15:36:04 +0530

    +fix

--- app/components/docs/feedback/CoreDialog.docs.js
+++ app/components/docs/feedback/CoreDialog.docs.js
@@ -1,43 +1,32 @@
+import React from "react";
+
 import {
   CoreH4,
   CoreTypographyBody1,
-  CoreDivider,
   CoreDialog,
   CoreStack,
-  CoreTypographyButton,
-  CoreTypographySubtitle1,
-  CoreCircularProgress,
   // CoreSimpleDialog
   // CoreDialogTitle
 } from "@wrappid/core";
+
 import CodeSample from "../../CodeSample";
 import ComponentProps from "../../ComponentProps";
-import React from "react";
 
 export default function CoreDialogDocs() {
-  // const [open, setOpen] = React.useState(false);
-  // const handleClose = () => {
-  //   setOpen(false);
-  // };
-  // const handleOpen = () => {
-  //   setOpen(true);
-  // };
-//   const { onClose, selectedValue, open } = props;
+  const [open, setOpen] = React.useState(false);
+  const [selectedValue, setSelectedValue] = React.useState("abc" /*emails[1]*/);
+  // const { onClose} = props;
+  //   const handleListItemClick = (value) => {
+  //     onClose(value);
+  //   };
 
-const [open, setOpen] = React.useState(false);
-const [selectedValue, setSelectedValue] = React.useState("abc"/*emails[1]*/);
-// const { onClose} = props;
-//   const handleListItemClick = (value) => {
-//     onClose(value);
-//   };
-
-//   const handleClickOpen = () => {
-//     setOpen(true);
-//   };
-//   const handleClose = (value) => {
-//     setOpen(false);
-//     setSelectedValue(value);
-//   };
+  //   const handleClickOpen = () => {
+  //     setOpen(true);
+  //   };
+  //   const handleClose = (value) => {
+  //     setOpen(false);
+  //     setSelectedValue(value);
+  //   };
 
   return (
     <>
@@ -46,59 +35,39 @@ const [selectedValue, setSelectedValue] = React.useState("abc"/*emails[1]*/);
       </CoreDialog>
 
       <CoreH4>CoreDialog</CoreH4>
+
       <CoreStack direction="column" spacing={2}>
         <CoreTypographyBody1>
-          The Backdrop component narrows the user's focus to a particular
-          element on the screen.
+          Dialogs inform users about a task and can contain critical
+          information, require decisions, or involve multiple tasks.
         </CoreTypographyBody1>
+
         <CoreTypographyBody1>
-          The Backdrop signals a state change within the application and can be
-          used for creating loaders, dialogs, and more. In its simplest form,
-          the Backdrop component will add a dimmed layer over your application.
+          A Dialog is a type of modal window that appears in front of app
+          content to provide critical information or ask for a decision. Dialogs
+          disable all app functionality when they appear, and remain on screen
+          until confirmed, dismissed, or a required action has been taken.
+        </CoreTypographyBody1>
+
+        <CoreTypographyBody1>
+          Dialogs are purposefully interruptive, so they should be used
+          sparingly.
         </CoreTypographyBody1>
       </CoreStack>
 
       <CodeSample
-        title={"Basic alerts"}
-        description={
-          "The CoreDialog offers four severity levels that set a distinctive icon and color"
-        }
-        code={`
-const [open, setOpen] = React.useState(false);
-const handleClose = () => {
-    setOpen(false);
-};
-const handleOpen = () => {
-    setOpen(true);
-};
-return (
-    <>
-<CoreTypographyButton onClick={handleOpen}>Show backdrop</CoreTypographyButton>
-<CoreDialog
-open={open}
-onClick={handleClose}
->
-<CoreCircularProgress color="inherit" />
-</CoreDialog>
-</>
-)`}
-        renderElement={
-          <CoreStack direction="column" spacing={2}>
-            <CoreTypographySubtitle1>
-              Selected: {selectedValue}
-            </CoreTypographySubtitle1>
-            <br />
-            <CoreTypographyButton variant="outlined" /*onClick={handleClickOpen}*/>
-              Open simple dialog
-            </CoreTypographyButton>
-            {/* <CoreSimpleDialog
-              selectedValue={selectedValue}
-              open={open}
-              //onClose={handleClose}
-            /> */}
-          </CoreStack>
-        }
+        title={"Basic dialog"}
+        description={`
+Simple dialogs can provide additional details or actions about a list item. For example, they can display avatars, icons, clarifying subtext, or orthogonal actions (such as adding an account).
+
+Touch mechanics:
+
+Choosing an option immediately commits the option and closes the menu
+Touching outside of the dialog, or pressing Back, cancels the action and closes the dialog
+ `}
+        renderElement={<></>}
       />
+
       <ComponentProps component={CoreDialog} />
     </>
   );
