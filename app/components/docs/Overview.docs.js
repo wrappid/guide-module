96b4f8ff5a9746d52a8dfb02299f00fbc6e57458
Nilotpal Saha <nilotpal.saha00@gmail.com>
2023-10-26 15:26:37 +0530

    +import sample

--- app/components/docs/Overview.docs.js
+++ app/components/docs/Overview.docs.js
@@ -1,7 +1,15 @@
 import { CoreH4 } from "@wrappid/core";
 
+import CodeImport from "../CodeImport";
+
 export default function OverviewDocs() {
   return (
-    <CoreH4>Overview Docs</CoreH4>
+    <>
+      <CoreH4>Overview Docs</CoreH4>
+
+      <CodeImport name="SingleComponent" />
+
+      <CodeImport name={["Component1", "Component2", "Component3"]} />
+    </>
   );
 }
