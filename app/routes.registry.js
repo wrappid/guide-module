6a7bf43d98e87cc54c5a3595b2f2f9ecef0a0b89
Nilotpal Saha <nilotpal.saha00@gmail.com>
2023-11-04 10:56:03 +0530

    + guide module updated with wrappid module and routes

---
+++ app/routes.registry.js
@@ -0,0 +1,14 @@
+export const RoutesRegistry = {
+  components: {
+    Page        : { appComponent: "Components" },
+    authRequired: false,
+    entityRef   : "components",
+    url         : "guide/components"
+  },
+  styleUtilities: {
+    Page        : { appComponent: "StyleUtilities" },
+    authRequired: false,
+    entityRef   : "styles",
+    url         : "guide/styles"
+  },
+};
