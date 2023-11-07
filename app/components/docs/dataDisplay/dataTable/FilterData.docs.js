2671e9b3fcaf26d2296ee909e4c25c2dac2d884c
Pritam Mondal <pritammondal8981@gmail.com>
2023-11-07 20:37:35 +0530

    + change folder structure same as core

---
+++ app/components/docs/dataDisplay/dataTable/FilterData.docs.js
@@ -0,0 +1,28 @@
+import {
+  CoreH4,
+  CoreTypographyBody1
+} from "@wrappid/core";
+  
+import CodeSample from "../../../CodeSample";
+
+export default function FilterDataDocs() {
+  return (
+    <>
+      <CoreH4>FilterData</CoreH4>
+  
+      <CoreTypographyBody1>
+          COMPONENT_DESCRIPTION
+      </CoreTypographyBody1>
+  
+      <CodeSample
+        title={"TITLE_OF_THE_SAMPLE (NOT_DEFINED)"}
+        description={"DESCRIPTION_OF_THE_SAMPLE"}
+        code={"PRE-FORMATTED_CODE_GOES_HERE"}
+        renderElement={<></>}
+      />
+        
+      {/* <ComponentProps component={FilterData} /> */}
+      
+    </>
+  );
+}
---
+++ app/components/docs/inputs/forms/CoreFormContainer.docs.js
@@ -0,0 +1,28 @@
+import {
+  CoreH4,
+  CoreTypographyBody1
+} from "@wrappid/core";
+  
+import CodeSample from "../../../CodeSample";
+
+export default function CoreFormContainerDocs() {
+  return (
+    <>
+      <CoreH4>CoreFormContainer</CoreH4>
+  
+      <CoreTypographyBody1>
+          COMPONENT_DESCRIPTION
+      </CoreTypographyBody1>
+  
+      <CodeSample
+        title={"TITLE_OF_THE_SAMPLE (NOT_DEFINED)"}
+        description={"DESCRIPTION_OF_THE_SAMPLE"}
+        code={"PRE-FORMATTED_CODE_GOES_HERE"}
+        renderElement={<></>}
+      />
+        
+      {/* <ComponentProps component={CoreFormContainer} /> */}
+      
+    </>
+  );
+}
