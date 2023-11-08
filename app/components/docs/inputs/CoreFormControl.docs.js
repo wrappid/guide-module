9cba824e7355313a1efbde04938602390eaab345
Pritam Mondal <pritammondal8981@gmail.com>
2023-11-08 23:39:10 +0530

    + Codesample CoreFromControl

--- app/components/docs/inputs/CoreFormControl.docs.js
+++ app/components/docs/inputs/CoreFormControl.docs.js
@@ -1,5 +1,6 @@
+import * as React from "react";
+
 import {
-  CoreFormControl,
   CoreH4,
   CoreTypographyBody1
 } from "@wrappid/core";
@@ -7,6 +8,22 @@ import {
 import CodeSample from "../../CodeSample";
 
 export default function CoreFormControlDocs() {
+  const [state, setState] = React.useState({
+    antoine: false,
+    gilad  : true,
+    jason  : false,
+  });
+
+  const handleChange = (event) => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.checked,
+    });
+  };
+
+  const { gilad, jason, antoine } = state;
+  const error = [gilad, jason, antoine].filter((v) => v).length !== 2;
+
   return (
     <>
       <CoreH4>CoreFormControl</CoreH4>
@@ -22,7 +39,69 @@ export default function CoreFormControlDocs() {
 <CoreFormControl color="secondary"></CoreFormControl>
         `}
         renderElement={<>
-          <CoreFormControl color="secondary"></CoreFormControl>
+          {/* <CoreBox sx={{ display: "flex" }}>
+            <CoreFormControl sx={{ m: 3 }} component="fieldset" variant="standard">
+
+              <CoreFormGroup>
+                <CoreFormControlLabel
+                  control={
+                    <CoreCheckbox checked={gilad} onChange={handleChange} name="gilad" />
+                  }
+                  label="Gilad Gray"
+                />
+
+                <CoreFormControlLabel
+                  control={
+                    <CoreCheckbox checked={jason} onChange={handleChange} name="jason" />
+                  }
+                  label="Jason Killian"
+                />
+
+                <CoreFormControlLabel
+                  control={
+                    <CoreCheckbox checked={antoine} onChange={handleChange} name="antoine" />
+                  }
+                  label="Antoine Llorca"
+                />
+              </CoreFormGroup>
+
+              <CoreFormHelperText>Be careful</CoreFormHelperText>
+            </CoreFormControl>
+
+            <CoreFormControl
+              required
+              error={error}
+              component="fieldset"
+              sx={{ m: 3 }}
+              variant="standard"
+            >
+
+              <CoreFormGroup>
+                <CoreFormControlLabel
+                  control={
+                    <CoreCheckbox checked={gilad} onChange={handleChange} name="gilad" />
+                  }
+                  label="Gilad Gray"
+                />
+
+                <CoreFormControlLabel
+                  control={
+                    <CoreCheckbox checked={jason} onChange={handleChange} name="jason" />
+                  }
+                  label="Jason Killian"
+                />
+
+                <CoreFormControlLabel
+                  control={
+                    <CoreCheckbox checked={antoine} onChange={handleChange} name="antoine" />
+                  }
+                  label="Antoine Llorca"
+                />
+              </CoreFormGroup>
+
+              <CoreFormHelperText>You can display an error</CoreFormHelperText>
+            </CoreFormControl>
+          </CoreBox> */}
         </>}
       />
         
