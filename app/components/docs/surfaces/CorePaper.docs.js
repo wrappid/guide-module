3976564a6803edf4e0b981a96daf6444f1ab651f
Pritam Mondal <pritammondal8981@gmail.com>
2023-11-09 14:31:49 +0530

    +codeSample

--- app/components/docs/surfaces/CorePaper.docs.js
+++ app/components/docs/surfaces/CorePaper.docs.js
@@ -1,5 +1,7 @@
 import {
+  CoreBox,
   CoreH4,
+  CorePaper,
   CoreTypographyBody1
 } from "@wrappid/core";
   
@@ -15,12 +17,65 @@ export default function CorePaperDocs() {
       </CoreTypographyBody1>
   
       <CodeSample
-        title={"TITLE_OF_THE_SAMPLE (NOT_DEFINED)"}
-        description={"DESCRIPTION_OF_THE_SAMPLE"}
+        title={"Introduction CorePaper (NOT_WORKING)<SX>"}
+        description={"In Material Design, surface components and shadow styles are heavily influenced by their real-world physical counterparts.\
+        Material UI implements this concept with the Paper component, a container-like surface that features the elevation prop for pulling box-shadow values from the theme."}
+        code={"PRE-FORMATTED_CODE_GOES_HERE"}
+        renderElement={<>
+          <CoreBox
+            sx={{
+              "& > :not(style)": {
+                height: 128,
+                m     : 1,
+                width : 128,
+              },
+              display : "flex",
+              flexWrap: "wrap",
+            }}
+          >
+            <CorePaper elevation={0} />
+
+            <CorePaper />
+
+            <CorePaper elevation={3} />
+          </CoreBox>
+        </>}
+      />
+
+      <CodeSample
+        title={"Elevation (NOT_WORKING)"}
+        description={"Use the elevation prop to establish hierarchy through the use of shadows. The Paper component's default elevation level is 1. The prop accepts values from 0 to 24. The higher the number, the further away the Paper appears to be from its background.\
+        In dark mode, increasing the elevation also makes the background color lighter. This is done by applying a semi-transparent gradient with the background-image CSS property."}
         code={"PRE-FORMATTED_CODE_GOES_HERE"}
         renderElement={<></>}
       />
-        
+
+      <CodeSample
+        title={"Variants (NOT_WORKING)"}
+        description={"Set the variant prop to \"outlined\" for a flat, outlined Paper with no shadows:"}
+        code={"PRE-FORMATTED_CODE_GOES_HERE"}
+        renderElement={<>
+          {/* <CoreStack direction="row" spacing={2}>
+            <CoreDemoPaper variant="elevation">default variant</CoreDemoPaper>
+
+            <CoreDemoPaper variant="outlined">outlined variant</CoreDemoPaper>
+          </CoreStack> */}
+        </>}
+      />
+
+      <CodeSample
+        title={"Corners (NOT_WORKING)"}
+        description={"DESCRIPTION_OF_THE_SAMPLE"}
+        code={"PRE-FORMATTED_CODE_GOES_HERE"}
+        renderElement={<>
+          {/* <CoreStack direction="row" spacing={2}>
+            <CoreDemoPaper square={false}>rounded corners</CoreDemoPaper>
+
+            <CoreDemoPaper square>square corners</CoreDemoPaper>
+          </CoreStack> */}
+        </>}
+      />
+
       {/* <ComponentProps component={CorePaper} /> */}
       
     </>
--- app/components/docs/utils/CoreModal.docs.js
+++ app/components/docs/utils/CoreModal.docs.js
@@ -1,22 +1,84 @@
+import * as React from "react";
+
 import {
+  CoreBox,
+  CoreButton,
   CoreH4,
+  CoreModal,
   CoreTypographyBody1
 } from "@wrappid/core";
   
 import CodeSample from "../../CodeSample";
 
 export default function CoreModalDocs() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <>
       <CoreH4>CoreModal</CoreH4>
   
       <CoreTypographyBody1>
-          COMPONENT_DESCRIPTION
+      The modal component provides a solid foundation for creating dialogs, popovers, lightboxes, or whatever else.
       </CoreTypographyBody1>
-  
+      Basic modal
+
       <CodeSample
-        title={"TITLE_OF_THE_SAMPLE (NOT_DEFINED)"}
+        title={"Basic CoreModal (NOT_WORKING)"}
         description={"DESCRIPTION_OF_THE_SAMPLE"}
+        code={"PRE-FORMATTED_CODE_GOES_HERE"}
+        renderElement={<>
+          <CoreBox>
+            <CoreButton onClick={handleOpen}>Open modal</CoreButton>
+
+            <CoreModal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <CoreBox >
+                <CoreTypographyBody1 id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+                </CoreTypographyBody1>
+
+                <CoreTypographyBody1 id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                </CoreTypographyBody1>
+              </CoreBox>
+            </CoreModal>
+          </CoreBox>
+        
+        </>}
+      />
+
+      <CodeSample
+        title={"Nested modal (NOT_WORKING)"}
+        description={"Modals can be nested, for example a select within a dialog, but stacking of more than two modals, or any two modals with a backdrop is discouraged"}
+        code={"PRE-FORMATTED_CODE_GOES_HERE"}
+        renderElement={<>
+
+        </>}
+      />
+
+      <CodeSample
+        title={"Transitions (NOT_WORKING)"}
+        description={"The open/close state of the modal can be animated with a transition component. This component should respect the following conditions:"}
+        code={"PRE-FORMATTED_CODE_GOES_HERE"}
+        renderElement={<></>}
+      />
+
+      <CodeSample
+        title={"Performance (NOT_WORKING)"}
+        description={"The content of modal is unmounted when closed. If you need to make the content available to search engines or render expensive component trees inside your modal while optimizing for interaction responsiveness it might be a good idea to change this default behavior by enabling the keepMounted prop:"}
+        code={"PRE-FORMATTED_CODE_GOES_HERE"}
+        renderElement={<></>}
+      />
+
+      <CodeSample
+        title={"Server-side modal (NOT_WORKING)"}
+        description={"React doesn't support the createPortal() API on the server. In order to display the modal, you need to disable the portal feature with the disablePortal prop:"}
         code={"PRE-FORMATTED_CODE_GOES_HERE"}
         renderElement={<></>}
       />
