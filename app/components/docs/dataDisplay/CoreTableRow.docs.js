a4903673af6411ca14d90c1d077697659a1417d1
Pritam Mondal <pritammondal8981@gmail.com>
2023-11-09 11:36:21 +0530

    +codesample

--- app/components/docs/dataDisplay/CoreTableRow.docs.js
+++ app/components/docs/dataDisplay/CoreTableRow.docs.js
@@ -1,5 +1,12 @@
 import {
+  CoreClasses,
   CoreH4,
+  CoreTable,
+  CoreTableBody,
+  CoreTableCell,
+  CoreTableContainer,
+  CoreTableHead,
+  CoreTableRow,
   CoreTypographyBody1
 } from "@wrappid/core";
   
@@ -15,14 +22,101 @@ export default function CoreTableRowDocs() {
       </CoreTypographyBody1>
   
       <CodeSample
-        title={"TITLE_OF_THE_SAMPLE (NOT_DEFINED)"}
-        description={"DESCRIPTION_OF_THE_SAMPLE"}
-        code={"PRE-FORMATTED_CODE_GOES_HERE"}
-        renderElement={<></>}
+        title={"Basic CoreTableRow"}
+        description={"A simple example with no frills."}
+        code={`<CoreTableContainer >
+<CoreTable styleClasses={[CoreClasses.WIDTH.MIN_W_50]} aria-label="simple table">
+  <CoreTableHead>
+    <CoreTableRow>
+      <CoreTableCell>Dessert (100g serving)</CoreTableCell>
+
+      <CoreTableCell align="right">Calories</CoreTableCell>
+
+      <CoreTableCell align="right">Fat&nbsp;(g)</CoreTableCell>
+
+      <CoreTableCell align="right">Carbs&nbsp;(g)</CoreTableCell>
+
+      <CoreTableCell align="right">Protein&nbsp;(g)</CoreTableCell>
+    </CoreTableRow>
+  </CoreTableHead>
+
+  <CoreTableBody>
+    {rows.map((row) => (
+      <CoreTableRow
+        key={row.name}
+      >
+        <CoreTableCell component="th" scope="row">
+          {row.name}
+        </CoreTableCell>
+
+        <CoreTableCell align="right">{row.calories}</CoreTableCell>
+
+        <CoreTableCell align="right">{row.fat}</CoreTableCell>
+
+        <CoreTableCell align="right">{row.carbs}</CoreTableCell>
+
+        <CoreTableCell align="right">{row.protein}</CoreTableCell>
+      </CoreTableRow>
+    ))}
+  </CoreTableBody>
+</CoreTable>
+</CoreTableContainer>`}
+        renderElement={<>
+          <CoreTableContainer >
+            <CoreTable styleClasses={[CoreClasses.WIDTH.MIN_W_50]} aria-label="simple table">
+              <CoreTableHead>
+                <CoreTableRow>
+                  <CoreTableCell>Dessert (100g serving)</CoreTableCell>
+
+                  <CoreTableCell align="right">Calories</CoreTableCell>
+
+                  <CoreTableCell align="right">Fat&nbsp;(g)</CoreTableCell>
+
+                  <CoreTableCell align="right">Carbs&nbsp;(g)</CoreTableCell>
+
+                  <CoreTableCell align="right">Protein&nbsp;(g)</CoreTableCell>
+                </CoreTableRow>
+              </CoreTableHead>
+
+              <CoreTableBody>
+                {rows.map((row) => (
+                  <CoreTableRow
+                    key={row.name}
+                    // sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <CoreTableCell component="th" scope="row">
+                      {row.name}
+                    </CoreTableCell>
+
+                    <CoreTableCell align="right">{row.calories}</CoreTableCell>
+
+                    <CoreTableCell align="right">{row.fat}</CoreTableCell>
+
+                    <CoreTableCell align="right">{row.carbs}</CoreTableCell>
+
+                    <CoreTableCell align="right">{row.protein}</CoreTableCell>
+                  </CoreTableRow>
+                ))}
+              </CoreTableBody>
+            </CoreTable>
+          </CoreTableContainer>
+        </>}
       />
-        
+
       {/* <ComponentProps component={CoreTableRow} /> */}
       
     </>
   );
 }
+
+function createData(name, calories, fat, carbs, protein) {
+  return { calories, carbs, fat, name, protein };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+];
