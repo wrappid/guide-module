48cd18ae28d7522c4203d23f2d7f60e30167a8d9
Pritam Mondal <pritammondal8981@gmail.com>
2023-11-09 12:13:22 +0530

    ++ edit CodeSample

--- app/components/docs/navigation/CoreLink.docs.js
+++ app/components/docs/navigation/CoreLink.docs.js
@@ -1,9 +1,13 @@
 import {
+  CoreClasses,
   CoreH4,
+  CoreLink,
+  CoreStack,
   CoreTypographyBody1
 } from "@wrappid/core";
   
 import CodeSample from "../../CodeSample";
+const preventDefault = (event) => event.preventDefault();
 
 export default function CoreLinkDocs() {
   return (
@@ -11,11 +15,93 @@ export default function CoreLinkDocs() {
       <CoreH4>CoreLink</CoreH4>
   
       <CoreTypographyBody1>
-          COMPONENT_DESCRIPTION
+      The Link component allows you to easily customize anchor elements with your theme colors and typography styles.
       </CoreTypographyBody1>
   
       <CodeSample
-        title={"TITLE_OF_THE_SAMPLE (NOT_DEFINED)"}
+        title={"Basic Corelinks (NOT_WORKING)"}
+        description={"The Link component is built on top of the Typography component, meaning that you can use its props."}
+        code={` <CoreStack
+  styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER]}
+  spacing={2}
+  direction="row"
+  onClick={preventDefault}
+>
+  <CoreLink href="#">Link</CoreLink>
+
+  <CoreLink href="#" color="inherit">
+    {"color=\"inherit\""}
+  </CoreLink>
+
+  <CoreLink href="#" variant="body2">
+    {"variant=\"body2\""}
+  </CoreLink>
+</CoreStack>`}
+        renderElement={<>
+          <CoreStack
+            styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER]}
+            spacing={2}
+            direction="row"
+            onClick={preventDefault}
+          >
+            <CoreLink href="#">Link</CoreLink>
+
+            <CoreLink href="#" color="inherit">
+              {"color=\"inherit\""}
+            </CoreLink>
+
+            <CoreLink href="#" variant="body2">
+              {"variant=\"body2\""}
+            </CoreLink>
+          </CoreStack>
+        </>}
+      />
+
+      <CodeSample
+        title={"Underline (NOT_WORKING)"}
+        description={"The underline prop can be used to set the underline behavior. The default is always."}
+        code={`<CoreStack
+  styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER]}
+  spacing={2}
+  direction="row"
+  onClick={preventDefault}
+>
+  <CoreLink href="#" underline="none">
+    {"underline=\"none\""}
+  </CoreLink>
+
+  <CoreLink href="#" underline="hover">
+    {"underline=\"hover\""}
+  </CoreLink>
+
+  <CoreLink href="#" underline="always">
+    {"underline=\"always\""}
+  </CoreLink>
+</CoreStack>`}
+        renderElement={<>
+          <CoreStack
+            styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER]}
+            spacing={2}
+            direction="row"
+            onClick={preventDefault}
+          >
+            <CoreLink href="#" underline="none">
+              {"underline=\"none\""}
+            </CoreLink>
+
+            <CoreLink href="#" underline="hover">
+              {"underline=\"hover\""}
+            </CoreLink>
+
+            <CoreLink href="#" underline="always">
+              {"underline=\"always\""}
+            </CoreLink>
+          </CoreStack>
+        </>}
+      />
+
+      <CodeSample
+        title={"Security(NOT_DEFINED)"}
         description={"DESCRIPTION_OF_THE_SAMPLE"}
         code={"PRE-FORMATTED_CODE_GOES_HERE"}
         renderElement={<></>}
