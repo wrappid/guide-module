9e09d41d7e4f58f32a2f4f479a72675077de424f
Iftikar Alam <edithmark42@gmail.com>
2023-11-18 18:38:22 +0530

    CoreRating documentation Added #4

--- app/components/component-docs/inputs/CoreRating.docs.js
+++ app/components/component-docs/inputs/CoreRating.docs.js
@@ -1,25 +1,300 @@
 import {
   CoreH4,
-  CoreTypographyBody1
-  // CoreRating
+  CoreTypographyBody1,
+  CoreRating,
+  CoreBox,
 } from "@wrappid/core";
+import React from "react";
+import PropTypes from "prop-types";
+import CodeSample from "../git/guide-module/app/components/CodeSample";
+import Rating from "@mui/material/Rating";
+import { styled } from "@mui/material/styles";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import StarIcon from "@mui/icons-material/Star";
+import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
+import SentimentDissatisfiedIcon from "@mui/icons-material/SentimentDissatisfied";
+import SentimentSatisfiedIcon from "@mui/icons-material/SentimentSatisfied";
+import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAltOutlined";
+import SentimentVerySatisfiedIcon from "@mui/icons-material/SentimentVerySatisfied";
 
-import CodeSample from "../../CodeSample";
+// labels for hover Feedback Section
+const labels = {
+  0.5: "Useless",
+  1: "Useless+",
+  1.5: "Poor",
+  2: "Poor+",
+  2.5: "Ok",
+  3: "Ok+",
+  3.5: "Good",
+  4: "Good+",
+  4.5: "Excellent",
+  5: "Excellent+",
+};
 
+// getLabelText for hover Feedback Section
+function getLabelText(value) {
+  return `${value} Star${value !== 1 ? "s" : ""}, ${labels[value]}`;
+}
+
+// StyledRating for the Customization Section
+const StyledRating = styled(Rating)({
+  "& .MuiRating-iconFilled": {
+    color: "#ff6d75",
+  },
+  "& .MuiRating-iconHover": {
+    color: "#ff3d47",
+  },
+});
+
+// StyledRating1 for the Radio Section
+const StyledRating1 = styled(Rating)(({ theme }) => ({
+  "& .MuiRating-iconEmpty .MuiSvgIcon-root": {
+    color: theme.palette.action.disabled,
+  },
+}));
+
+// customIcons for Radio Section
+const customIcons = {
+  1: {
+    icon: <SentimentVeryDissatisfiedIcon color="error" />,
+    label: "Very Dissatisfied",
+  },
+  2: {
+    icon: <SentimentDissatisfiedIcon color="error" />,
+    label: "Dissatisfied",
+  },
+  3: {
+    icon: <SentimentSatisfiedIcon color="warning" />,
+    label: "Neutral",
+  },
+  4: {
+    icon: <SentimentSatisfiedAltIcon color="success" />,
+    label: "Satisfied",
+  },
+  5: {
+    icon: <SentimentVerySatisfiedIcon color="success" />,
+    label: "Very Satisfied",
+  },
+};
+
+// IconContainer for Radio Section
+function IconContainer(props) {
+  const { value, ...other } = props;
+  return <span {...other}>{customIcons[value].icon}</span>;
+}
+
+IconContainer.propTypes = {
+  value: PropTypes.number.isRequired,
+};
+
+// Default export function
 export default function CoreRatingDocs() {
+  const [value, setValue] = React.useState(3);
+  const [hover, setHover] = React.useState(-1);
+
   return (
     <>
       <CoreH4>CoreRating</CoreH4>
 
       <CoreTypographyBody1>
-        COMPONENT_DESCRIPTION
+        Ratings provide insight regarding others' opinions and experiences, and
+        can allow the user to submit a rating of their own.
       </CoreTypographyBody1>
 
       <CodeSample
-        title={"TITLE_OF_THE_SAMPLE"}
+        title={"Basic Rating"}
         description={"DESCRIPTION_OF_THE_SAMPLE"}
-        code={"PRE-FORMATTED_CODE_GOES_HERE"}
-        renderElement={<></>}
+        code={`<CoreTypographyBody1>Controlled </CoreTypographyBody1>
+        <CoreRating
+          name="simple-controlled"
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        />
+       
+        <CoreTypographyBody1>Read only </CoreTypographyBody1>
+        <CoreRating name="read-only" value={value} readOnly />
+        
+        <CoreTypographyBody1>Disabled </CoreTypographyBody1>
+        <CoreRating name="disabled" value={value} disabled />
+
+        <CoreTypographyBody1>No rating given </CoreTypographyBody1>
+        <CoreRating name="no-value" value={null} />`}
+        renderElement={
+          <>
+            <CoreTypographyBody1>Controlled </CoreTypographyBody1>
+            <CoreRating
+              name="simple-controlled"
+              value={value}
+              onChange={(event, newValue) => {
+                setValue(newValue);
+              }}
+            />
+
+            <CoreTypographyBody1>Read only </CoreTypographyBody1>
+            <CoreRating name="read-only" value={value} readOnly />
+
+            <CoreTypographyBody1>Disabled </CoreTypographyBody1>
+            <CoreRating name="disabled" value={value} disabled />
+
+            <CoreTypographyBody1>No rating given </CoreTypographyBody1>
+            <CoreRating name="no-value" value={null} />
+          </>
+        }
+      />
+
+      <CodeSample
+        title={"Rating precision"}
+        description={
+          "The rating can display any float number with the value prop. Use the precision prop to define the minimum increment value change allowed."
+        }
+        code={`<CoreRating name="half-rating" defaultValue={2.5} precision={0.5} />
+        <CoreRating name="half-rating-read" defaultValue={2.5} precision={0.5} readOnly />`}
+        renderElement={
+          <>
+            <CoreRating name="half-rating" defaultValue={2.5} precision={0.5} />
+            <CoreRating
+              name="half-rating-read"
+              defaultValue={2.5}
+              precision={0.5}
+              readOnly
+            />
+          </>
+        }
+      />
+
+      <CodeSample
+        title={"Hover feedback"}
+        description={
+          "You can display a label on hover to help the user pick the correct rating value. The demo uses the onChangeActive prop."
+        }
+        code={`<CoreRating
+          name="hover-feedback"
+          value={value}
+          precision={0.5}
+          getLabelText={getLabelText}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+          onChangeActive={(event, newHover) => {
+            setHover(newHover);
+          }}
+          emptyIcon={
+            <StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />
+          }
+        />
+        {value !== null && (
+          <CoreBox sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : value]}</CoreBox>
+        )}`}
+        renderElement={
+          <>
+            <CoreRating
+              name="hover-feedback"
+              value={value}
+              precision={0.5}
+              getLabelText={getLabelText}
+              onChange={(event, newValue) => {
+                setValue(newValue);
+              }}
+              onChangeActive={(event, newHover) => {
+                setHover(newHover);
+              }}
+              emptyIcon={
+                <StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />
+              }
+            />
+            {value !== null && (
+              <CoreBox sx={{ ml: 2 }}>
+                {labels[hover !== -1 ? hover : value]}
+              </CoreBox>
+            )}
+          </>
+        }
+      />
+
+      <CodeSample
+        title={"Sizes"}
+        description={"For larger or smaller ratings use the size prop."}
+        code={`<CoreRating name="size-small" defaultValue={2} size="small" />
+        <CoreRating name="size-medium" defaultValue={2} />
+        <CoreRating name="size-large" defaultValue={2} size="large" />`}
+        renderElement={
+          <>
+            <CoreRating name="size-small" defaultValue={2} size="small" />
+            <CoreRating name="size-medium" defaultValue={2} />
+            <CoreRating name="size-large" defaultValue={2} size="large" />
+          </>
+        }
+      />
+
+      <CodeSample
+        title={"Customization"}
+        description={"Here are some examples of customizing the component."}
+        code={`<CoreTypographyBody1 component="legend">
+        Custom icon and color
+      </CoreTypographyBody1>
+      <StyledRating
+        name="customized-color"
+        defaultValue={2}
+        getLabelText={(value: number) =>
+          \`\${value} Heart\${value !== 1 ? "s" : ""}\`
+        }
+        precision={0.5}
+        icon={<FavoriteIcon fontSize="inherit" />}
+        emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+      />
+      <CoreTypographyBody1 component="legend">
+        10 stars
+      </CoreTypographyBody1>
+      <CoreRating name="customized-10" defaultValue={2} max={10} />`}
+        renderElement={
+          <>
+            <CoreTypographyBody1 component="legend">
+              Custom icon and color
+            </CoreTypographyBody1>
+            <StyledRating
+              name="customized-color"
+              defaultValue={2}
+              getLabelText={(value: number) =>
+                `${value} Heart${value !== 1 ? "s" : ""}`
+              }
+              precision={0.5}
+              icon={<FavoriteIcon fontSize="inherit" />}
+              emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+            />
+            <CoreTypographyBody1 component="legend">
+              10 stars
+            </CoreTypographyBody1>
+            <CoreRating name="customized-10" defaultValue={2} max={10} />
+          </>
+        }
+      />
+
+      <CodeSample
+        title={"Radio group"}
+        description={
+          "The rating is implemented with a radio group, set highlightSelectedOnly to restore the natural behavior."
+        }
+        code={`<StyledRating
+          name="highlight-selected-only"
+          defaultValue={2}
+          IconContainerComponent={IconContainer}
+          getLabelText={(value) => customIcons[value].label}
+          highlightSelectedOnly
+        />`}
+        renderElement={
+          <>
+            <StyledRating
+              name="highlight-selected-only"
+              defaultValue={2}
+              IconContainerComponent={IconContainer}
+              getLabelText={(value) => customIcons[value].label}
+              highlightSelectedOnly
+            />
+          </>
+        }
       />
 
       {/* <ComponentProps component={CoreRating} /> */}
