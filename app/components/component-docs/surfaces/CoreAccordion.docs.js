b4fac16fbda3b765103b5673fbf36c336fd8bbb6
Md Faisal Raza Khan <irkfaisal@gmail.com>
2023-12-21 17:45:10 +0530

    docs: :art: add CoreAccordion documentation

--- app/components/component-docs/surfaces/CoreAccordion.docs.js
+++ app/components/component-docs/surfaces/CoreAccordion.docs.js
@@ -1,6 +1,8 @@
 import {
   CoreH4,
-  CoreTypographyBody1
+  CoreTypographyBody1,
+  CoreStack, CoreAccordion, CoreAccordionSummary, CoreAccordionDetail, CoreTypographyBody2, CoreIcon,
+  CorePaper
 } from "@wrappid/core";
   
 import CodeSample from "../../CodeSample";
@@ -11,14 +13,70 @@ export default function CoreAccordionDocs() {
       <CoreH4>CoreAccordion</CoreH4>
   
       <CoreTypographyBody1>
-          COMPONENT_DESCRIPTION
+      The accordion component allows the user to show and hide sections of related content on a page.
+      An accordion is a lightweight container that may either be used standalone, or be connected to a larger surface, such as a card.
       </CoreTypographyBody1>
   
       <CodeSample
-        title={"TITLE_OF_THE_SAMPLE (NOT_DEFINED)"}
-        description={"DESCRIPTION_OF_THE_SAMPLE"}
-        code={"PRE-FORMATTED_CODE_GOES_HERE"}
-        renderElement={<></>}
+        title={"Basic accordion"}
+        description={"Expan icon is missing"}
+        code={` <CoreStack component={CorePaper}>
+        <CoreAccordion>
+          <CoreAccordionSummary expandIcon={<CoreIcon color="action" icon="arrow" />}>
+            <CoreTypographyBody1>Accordion 1</CoreTypographyBody1>
+          </CoreAccordionSummary>
+
+          <CoreAccordionDetail>
+            <CoreTypographyBody2> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.</CoreTypographyBody2>
+          </CoreAccordionDetail>
+        </CoreAccordion>
+        <CoreAccordion>
+          <CoreAccordionSummary expandIcon={<CoreIcon color="action" icon="arrow" />}>
+            <CoreTypographyBody1>Accordion 2</CoreTypographyBody1>
+          </CoreAccordionSummary>
+
+          <CoreAccordionDetail>
+            <CoreTypographyBody2>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.</CoreTypographyBody2>
+          </CoreAccordionDetail>
+        </CoreAccordion>
+        <CoreAccordion disabled>
+          <CoreAccordionSummary expandIcon={<CoreIcon color="action" icon="arrow" />}>
+            <CoreTypographyBody1>Disabled Accordion</CoreTypographyBody1>
+          </CoreAccordionSummary>
+        </CoreAccordion>
+      </CoreStack>`}
+        renderElement={
+        <>
+        <CoreStack component={CorePaper}>
+        <CoreAccordion>
+          <CoreAccordionSummary expandIcon={<CoreIcon color="action" icon="arrow" />}>
+            <CoreTypographyBody1>Accordion 1</CoreTypographyBody1>
+          </CoreAccordionSummary>
+
+          <CoreAccordionDetail>
+            <CoreTypographyBody2> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.</CoreTypographyBody2>
+          </CoreAccordionDetail>
+        </CoreAccordion>
+        <CoreAccordion>
+          <CoreAccordionSummary expandIcon={<CoreIcon color="action" icon="arrow" />}>
+            <CoreTypographyBody1>Accordion 2</CoreTypographyBody1>
+          </CoreAccordionSummary>
+
+          <CoreAccordionDetail>
+            <CoreTypographyBody2>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.</CoreTypographyBody2>
+          </CoreAccordionDetail>
+        </CoreAccordion>
+        <CoreAccordion disabled>
+          <CoreAccordionSummary expandIcon={<CoreIcon color="action" icon="arrow" />}>
+            <CoreTypographyBody1>Disabled Accordion</CoreTypographyBody1>
+          </CoreAccordionSummary>
+        </CoreAccordion>
+      </CoreStack>
+        </>}
       />
         
       {/* <ComponentProps component={CoreAccordion} /> */}
