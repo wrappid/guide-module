15d22e89af739f974bcae30684016704df4ac8e7
Md Faisal Raza Khan <irkfaisal@gmail.com>
2024-01-06 18:17:46 +0530

    fix: :bug: bugs related to CoreTOC
    change CoreAccordion to CoreMenu for table of content.
    
    ref:#7

--- app/components/component-docs/DocsTOC.js
+++ app/components/component-docs/DocsTOC.js
@@ -1,51 +1,82 @@
+import React from "react";
 import {
-  CoreAccordion, CoreAccordionDetail, CoreAccordionSummary, CoreBox, CoreClasses, CoreTextButton, CoreTypographyBody1 
+  toggleMenuItemState,
+  CoreAccordion,
+  CoreAccordionDetail,
+  CoreAccordionSummary,
+  CoreBox,
+  CoreClasses,
+  CoreTextButton,
+  CoreTypographyBody1,
+  CoreMenu,
 } from "@wrappid/core";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function DocsTOC(props) {
+  const dispatch = useDispatch();
+
+  const collapse = useSelector((state) => state?.menu?.collapse);
+  const [preparedMenu, setPreparedMenu] = React.useState([]);
   const { docsRegistry, /* currentPage, */ setCurrentPage } = props;
   const getSortedRegistry = (docsRegistry) => {
-    return Object.keys(docsRegistry).sort((curr, next) => docsRegistry[curr].order - docsRegistry[next].order);
-  };
-    
-  const renderTOC = (docsRegistry) => {
     return (
-      <>
-        {getSortedRegistry(docsRegistry).map(docKey => {
-          return (
-            <CoreAccordion
-              key={`${docKey}`}
-              styleClasses={[CoreClasses.BG.BG_SECONDARY]}
-              //   expanded={currentPage === docKey/*|| Object.keys(docsRegistry[docKey]?.children)?.includes(currentPage)*/}
-              //   onChange={setCurrentPage(docKey)}
-            >
-              <CoreAccordionSummary styleClasses={[CoreClasses.BG.BG_SUCCESS]}>
-                <CoreTextButton
-                  label={docKey}
-                  OnClick={() => {
-                    setCurrentPage(docKey);
-                  }}
-                />
-              </CoreAccordionSummary>
-
-              {docsRegistry[docKey]?.children && Object.keys(docsRegistry[docKey]?.children)?.length > 0 && (
-                <CoreAccordionDetail styleClasses={[CoreClasses.PADDING.PL1, CoreClasses.BG.BG_INFO]}>
-                  {renderTOC(docsRegistry[docKey]?.children)}
-                </CoreAccordionDetail>
-              )}
-            </CoreAccordion>
-          );
-        })}
-      </>
+      docsRegistry &&
+      Object.keys(docsRegistry)?.sort(
+        (curr, next) => docsRegistry[curr].order - docsRegistry[next].order
+      )
     );
   };
+
+  const prepareComponentMenu = (docsRegistry) => {
+    return (
+      getSortedRegistry(docsRegistry)?.map((docKey) => {
+        console.log("menu of", docKey);
+        console.log(prepareComponentMenu(docsRegistry[docKey]?.children));
+        return {
+          Children: prepareComponentMenu(docsRegistry[docKey]?.children),
+          id: docKey,
+          label: docKey?.trim(),
+          name: docKey?.trim(),
+          type: "menuitem",
+        };
+      }) || []
+    );
+  };
+  React.useEffect(() => {
+    setPreparedMenu(prepareComponentMenu(docsRegistry));
+  }, []);
+  React.useEffect(() => {
+    setPreparedMenu(prepareComponentMenu(docsRegistry));
+  }, [docsRegistry]);
+
+  React.useEffect(() => {
+    console.log("prepared menu");
+    console.log(preparedMenu);
+  }, [preparedMenu]);
 
   return (
     <>
       <CoreTypographyBody1>Table Of Contents</CoreTypographyBody1>
-     
-      <CoreBox CoreClasses={[CoreClasses.OVERFLOW.OVERFLOW_Y_SCROLL, CoreClasses.HEIGHT.H_100]}>
-        {renderTOC(docsRegistry)}
+
+      <CoreBox
+        CoreClasses={[
+          CoreClasses.OVERFLOW.OVERFLOW_Y_SCROLL,
+          CoreClasses.HEIGHT.H_100,
+        ]}
+      >
+        {/* {JSON.stringify(prepareComponentMenu(docsRegistry), null, 2)} */}
+        <CoreMenu
+          openCollapse={collapse}
+          multiLevel={true}
+          menu={prepareComponentMenu(docsRegistry)}
+          OnMenuClick={(menuItem) => {
+            if (menuItem.Children && menuItem.Children.length > 0) {
+              dispatch(toggleMenuItemState(menuItem));
+            }
+            setCurrentPage(menuItem.id);
+          }}
+          open={true}
+        />
       </CoreBox>
     </>
   );
