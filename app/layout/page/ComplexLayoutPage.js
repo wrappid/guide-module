7cf72faa25b603d68a38a6eb5f193ea63ee67852
Nilotpal Saha <nilotpal.saha00@gmail.com>
2024-03-11 14:17:45 +0530

    fix(core): :fire: complex layout page
    complex layout page
    
    Ref #109

--- app/layout/page/ComplexLayoutPage.js
+++ app/layout/page/ComplexLayoutPage.js
@@ -1,41 +1,54 @@
 // eslint-disable-next-line unused-imports/no-unused-imports, no-unused-vars
 import React from "react";
 
-import { CoreTypographyBody1, CoreLayoutItem } from "@wrappid/core";
+// eslint-disable-next-line import/no-unresolved
+import { CoreLayoutItem, CoreTypographyBody1 } from "@wrappid/core";
 
 export default function ComplexLayoutPage() {
   return (
     <>
       <CoreLayoutItem id="placeholder1">
-        <CoreTypographyBody1>Item 1</CoreTypographyBody1>
+        <CoreTypographyBody1>Layout Item - placeholder1</CoreTypographyBody1>
+
+        <CoreLayoutItem id="column1">
+          <CoreTypographyBody1>Two Column Layout - Layout Item - column1</CoreTypographyBody1>
+        </CoreLayoutItem>
+
+        <CoreLayoutItem id="column2">
+          <CoreTypographyBody1>Two Column Layout - Layout Item - column2</CoreTypographyBody1>
+        </CoreLayoutItem>
 
         <CoreLayoutItem id="placeholder4">
-          <CoreTypographyBody1>Item 4</CoreTypographyBody1>
+          <CoreTypographyBody1>Layout Item - placeholder4</CoreTypographyBody1>
         </CoreLayoutItem>
 
         <CoreLayoutItem id="placeholder5">
-          <CoreTypographyBody1>Item 5</CoreTypographyBody1>
+          <CoreTypographyBody1>Layout Item - placeholder5</CoreTypographyBody1>
+        </CoreLayoutItem>
 
-          <CoreLayoutItem id="placeholder7">
-            <CoreTypographyBody1>Item 7</CoreTypographyBody1>
-          </CoreLayoutItem>
+        <CoreLayoutItem id="placeholder7">
+          <CoreTypographyBody1>Layout Item - placeholder7</CoreTypographyBody1>
         </CoreLayoutItem>
       </CoreLayoutItem>
 
       <CoreLayoutItem id="placeholder2">
-        <CoreTypographyBody1>Item 2</CoreTypographyBody1>
+        <CoreTypographyBody1>Layout Item - placeholder2</CoreTypographyBody1>
 
         <CoreLayoutItem id="placeholder6">
-          <CoreTypographyBody1>Item 6</CoreTypographyBody1>
+          <CoreTypographyBody1>Layout Item - placeholder6</CoreTypographyBody1>
         </CoreLayoutItem>
 
         <CoreLayoutItem id="placeholder8">
-          <CoreTypographyBody1>Item 8</CoreTypographyBody1>
+          <CoreTypographyBody1>Layout Item - placeholder8</CoreTypographyBody1>
         </CoreLayoutItem>
       </CoreLayoutItem>
 
       <CoreLayoutItem id="placeholder3">
-        <CoreTypographyBody1>Item 3</CoreTypographyBody1>
+        <CoreTypographyBody1>Layout Item - placeholder3</CoreTypographyBody1>
+      </CoreLayoutItem>
+
+      <CoreLayoutItem id="placeholder9">
+        <CoreTypographyBody1>Layout Item - placeholder9</CoreTypographyBody1>
       </CoreLayoutItem>
     </>
   );
