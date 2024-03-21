9ab182a4676d2b534521bc5d35b5d2675aef953d
Md Faisal Raza Khan <irkfaisal@gmail.com>
2024-03-21 18:29:26 +0530

    docs(core): :lipstick: wrappid component's documentation UI
    wrappid component's documentation UI
    
    ref: #121

--- app/components/ComponentProps.js
+++ app/components/ComponentProps.js
@@ -1,66 +1,127 @@
+import React from "react";
+
 import {
-  defaultValidProps, defaultInvalidProps, CoreBox, CoreClasses, CoreH6, CoreTypographyBody1, CoreChip, CoreH5 
+  defaultValidProps, defaultInvalidProps, CoreBox, CoreClasses, CoreH6, CoreTypographyBody1, CoreChip, CoreH5, CoreDivider, CoreSelect, CoreGrid, CoreAccordion, CoreAccordionSummary, CoreAccordionDetail, CoreIcon, CoreIconButton
 } from "@wrappid/core";
 
 import ComponentPropTypes from "./ComponentPropTypes";
 
+const VIEWPROPSDATA = ["Table", "List"];
+
 export default function ComponentProps(props) {
+  const [viewPropsData, setViewPropsData] = React.useState(VIEWPROPSDATA[0]);
   const { component } = props;
-    
+
   const validProps = [...(component?.validProps || []), ...defaultValidProps];
   const invalidProps = [...(component?.invalidProps || []), ...defaultInvalidProps];
 
+  const handleSelectChange = (event) => {
+    setViewPropsData(event.target.value);
+  };
+
+  const PropsHeading = ({ title }) => {
+    return (
+      <CoreBox styleClasses={[CoreClasses.BG.BG_GREY_100, CoreClasses.PADDING.P2, CoreClasses.BORDER.BORDER_ROUNDED_1, CoreClasses.MARGIN.MY2]}>
+        <CoreH5>{title}</CoreH5>
+
+        {title === "Valid Props" && <CoreSelect
+          gridProps={{ gridSize: { md: 12 } }}
+          label="View props"
+          id="viewPropsData"
+          value={viewPropsData}
+          handleChange={(event) => handleSelectChange(event)}
+          options={Object.keys(VIEWPROPSDATA).map((key) => ({
+            id   : VIEWPROPSDATA[key],
+            label: VIEWPROPSDATA[key],
+            value: VIEWPROPSDATA[key],
+          }))}
+        />}
+      </CoreBox>
+    );
+  };
+
   return (
     <>
-      <CoreH6>{"Valid Props"}</CoreH6>
+      <PropsHeading title={"Valid Props"} />
 
       {validProps && validProps?.map(eachProp => {
         return (
-          <CoreBox key={`${eachProp}`} styleClasses={[CoreClasses.DISPLAY.FLEX, CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_FLEX_START, CoreClasses.ALIGNMENT.ALIGN_ITEMS_CENTER, CoreClasses.GAP.GAP_1]}>
+          <>
+            {<CoreBox key={`${eachProp}`} styleClasses={[CoreClasses.DISPLAY.FLEX, CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_FLEX_START, CoreClasses.ALIGNMENT.ALIGN_ITEMS_START, CoreClasses.GAP.GAP_2]}>
 
-            <CoreBox styleClasses={[
-              CoreClasses.DISPLAY.FLEX,
-              CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_FLEX_START,
-              CoreClasses.ALIGNMENT.ALIGN_ITEMS_CENTER,
-              CoreClasses.GAP.GAP_1,
-              CoreClasses.WIDTH.W_25
-            ]}>
-              <CoreH6 styleClasses={[CoreClasses.PADDING.PR1]}>{eachProp?.name}</CoreH6>
-                
-              {eachProp?.required ? (
-                <CoreChip size="small" color="info" label="REQUIRED" />
-              ) : (
-                <CoreChip size="small" color="warning" label="OPTIONAL" />
-              )}
-            </CoreBox>
+              {/* props data in list format */}
+              {viewPropsData === "Table" &&
+                <CoreGrid>
+                  <CoreBox gridProps={{ gridSize: { md: 3 } }}>
+                    <CoreH6 styleClasses={[CoreClasses.PADDING.PR1]}>{eachProp?.name}</CoreH6>
 
-            <CoreBox styleClasses={[CoreClasses.WIDTH.W_25]}>
-              
-              <CoreTypographyBody1>
-                {eachProp?.description}
-              </CoreTypographyBody1>
-            </CoreBox>
+                    {eachProp?.required ? (
+                      <CoreChip size="small" color="info" label="REQUIRED" />
+                    ) : (
+                      <CoreChip size="small" color="warning" label="OPTIONAL" />
+                    )}
+                  </CoreBox>
 
-            <CoreBox styleClasses={[CoreClasses.WIDTH.W_50]}>
+                  <CoreBox gridProps={{ gridSize: { md: 3 } }}>
 
-              <ComponentPropTypes propTypes={eachProp?.types} />
-            </CoreBox>
-          </CoreBox>
+                    <CoreTypographyBody1>
+                      {eachProp?.description}
+                    </CoreTypographyBody1>
+                  </CoreBox>
+
+                  <CoreBox gridProps={{ gridSize: { md: 6 } }}>
+
+                    <ComponentPropTypes propTypes={eachProp?.types} />
+                  </CoreBox>
+
+                  <CoreDivider />
+                </CoreGrid>
+              }
+
+              {/* props data in list format */}
+              {viewPropsData === "List" &&
+                <CoreGrid>
+                  <CoreAccordion gridProps={12}>
+                    <CoreAccordionSummary expandIcon={<CoreIconButton><CoreIcon icon="expand_more" /></CoreIconButton>}>
+                      <CoreH6>{eachProp?.name}</CoreH6>
+                    </CoreAccordionSummary>
+
+                    <CoreAccordionDetail>
+                      <CoreBox>
+                        {eachProp?.required ? (
+                          <CoreChip size="small" color="info" label="REQUIRED" />
+                        ) : (
+                          <CoreChip size="small" color="warning" label="OPTIONAL" />
+                        )}
+
+                        <CoreDivider />
+
+                        <CoreTypographyBody1>
+                          {eachProp?.description}
+                        </CoreTypographyBody1>
+
+                        <CoreDivider />
+
+                        <ComponentPropTypes propTypes={eachProp?.types} />
+                      </CoreBox>
+                    </CoreAccordionDetail>
+                  </CoreAccordion>
+                </CoreGrid>
+              }
+            </CoreBox>}
+          </>
         );
       })}
 
-      <CoreH6>{"Invalid Props"}</CoreH6>
+      <PropsHeading title={"Invalid Props"} />
 
-      {invalidProps && invalidProps?.map((eachProp, index) => {
+      {invalidProps && invalidProps?.map((eachProp) => {
         return (
           <CoreBox key={`${eachProp}`}>
-            <CoreH5>
-              {index + 1}.&nbsp;
+            <CoreH5>{eachProp}</CoreH5>
 
-              {eachProp}
-            </CoreH5>
+            <CoreDivider />
           </CoreBox>
-
         );
       })}
     </>
