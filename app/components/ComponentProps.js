758b709fc84c1c6228f69eaa8f0da92e0fd84bdf
Md Faisal Raza Khan <irkfaisal@gmail.com>
2024-03-21 13:36:58 +0530

    docs(core): :lipstick: change in the ui of valid props table
    change in the ui of valid props table
    
    ref: #121

--- app/components/ComponentProps.js
+++ app/components/ComponentProps.js
@@ -1,5 +1,5 @@
 import {
-  defaultValidProps, defaultInvalidProps, CoreBox, CoreClasses, CoreDivider, CoreH6, CoreTypographyBody1, CoreH5, CoreChip, CoreStack 
+  defaultValidProps, defaultInvalidProps, CoreBox, CoreClasses, CoreH6, CoreTypographyBody1, CoreChip, CoreH5 
 } from "@wrappid/core";
 
 import ComponentPropTypes from "./ComponentPropTypes";
@@ -12,41 +12,55 @@ export default function ComponentProps(props) {
 
   return (
     <>
-      <CoreH5>{"Valid Props"}</CoreH5>
+      <CoreH6>{"Valid Props"}</CoreH6>
 
       {validProps && validProps?.map(eachProp => {
         return (
-          <CoreBox key={`${eachProp}`}>
-            <CoreDivider styleClasses={[CoreClasses.MARGIN.MY3]} />
+          <CoreBox key={`${eachProp}`} styleClasses={[CoreClasses.DISPLAY.FLEX, CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_FLEX_START, CoreClasses.ALIGNMENT.ALIGN_ITEMS_CENTER, CoreClasses.GAP.GAP_1]}>
 
-            <CoreStack direction="row">
+            <CoreBox styleClasses={[
+              CoreClasses.DISPLAY.FLEX,
+              CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_FLEX_START,
+              CoreClasses.ALIGNMENT.ALIGN_ITEMS_CENTER,
+              CoreClasses.GAP.GAP_1,
+              CoreClasses.WIDTH.W_25
+            ]}>
               <CoreH6 styleClasses={[CoreClasses.PADDING.PR1]}>{eachProp?.name}</CoreH6>
                 
               {eachProp?.required ? (
-                <CoreChip size="small" color="primary" label="REQUIRED" />
+                <CoreChip size="small" color="info" label="REQUIRED" />
               ) : (
-                <CoreChip size="small" color="secondary" label="OPTIONAL" />
+                <CoreChip size="small" color="warning" label="OPTIONAL" />
               )}
-            </CoreStack>
+            </CoreBox>
 
-            <CoreTypographyBody1>
-              {eachProp?.description}
-            </CoreTypographyBody1>
+            <CoreBox styleClasses={[CoreClasses.WIDTH.W_25]}>
+              
+              <CoreTypographyBody1>
+                {eachProp?.description}
+              </CoreTypographyBody1>
+            </CoreBox>
 
-            <ComponentPropTypes propTypes={eachProp?.types} />
+            <CoreBox styleClasses={[CoreClasses.WIDTH.W_50]}>
+
+              <ComponentPropTypes propTypes={eachProp?.types} />
+            </CoreBox>
           </CoreBox>
         );
       })}
 
-      <CoreDivider />
+      <CoreH6>{"Invalid Props"}</CoreH6>
 
-      <CoreH5>{"Invalid Props"}</CoreH5>
-
-      {invalidProps && invalidProps?.map(eachProp => {
+      {invalidProps && invalidProps?.map((eachProp, index) => {
         return (
           <CoreBox key={`${eachProp}`}>
-            <CoreH6>{eachProp}</CoreH6>
+            <CoreH5>
+              {index + 1}.&nbsp;
+
+              {eachProp}
+            </CoreH5>
           </CoreBox>
+
         );
       })}
     </>
