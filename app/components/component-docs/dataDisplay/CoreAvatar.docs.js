f2fec58d9198f56a2ae95f112f1ba264ff928f90
PritamIT2023 <pritammondal8981@gmail.com>
2024-08-13 16:52:37 +0530

    docs(global): :bug: fix documentation for CoreAvatar
    fix documentation for CoreAvatar
    
    Ref: #58

--- app/components/component-docs/dataDisplay/CoreAvatar.docs.js
+++ app/components/component-docs/dataDisplay/CoreAvatar.docs.js
@@ -15,7 +15,6 @@ import CodeSample from "../../CodeSample";
 import ComponentProps from "../../ComponentProps";
 
 export default function CoreAvatarDocs() {
-
   return (
     <>
       <CoreH4 styleClasses={[CoreClasses.MARGIN.MY2, CoreClasses.COLOR.TEXT_PRIMARY]}>
@@ -38,6 +37,8 @@ export default function CoreAvatarDocs() {
             {" "}
 
             <CodeBlock>src</CodeBlock> or 
+
+            {" "}
 
             <CodeBlock>srcSet</CodeBlock> to the component.
           </>
@@ -67,15 +68,19 @@ export default function CoreAvatarDocs() {
         description={
           <>
             Avatars containing simple characters can be created by passing a string as
-            {/* eslint-disable-next-line react/jsx-newline */}
+            {" "}
+
             <CodeBlock>{"children"}</CodeBlock>.
           </>
         }
-        code={`<CoreStack styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER]} spacing={2} direction="row">
+        code={`<CoreStack
+  styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER]}
+  spacing={2}
+  direction="row"
+  >
   <CoreAvatar>W</CoreAvatar>
-  <CoreAvatar>WR</CoreAvatar>
-  <CoreAvatar styleClasses={[CoreClasses.BG.BG_PRIMARY]}>W</CoreAvatar>
-  <CoreAvatar styleClasses={[CoreClasses.BG.BG_SECONDARY]}>WR</CoreAvatar>
+  <CoreAvatar styleClasses={[CoreClasses.BG.BG_PRIMARY]}>WR</CoreAvatar>
+  <CoreAvatar styleClasses={[CoreClasses.BG.BG_SECONDARY]}>OP</CoreAvatar>
 </CoreStack>`}
         renderElement={
           <CoreStack
@@ -85,11 +90,9 @@ export default function CoreAvatarDocs() {
           >
             <CoreAvatar>W</CoreAvatar>
 
-            <CoreAvatar>WR</CoreAvatar>
+            <CoreAvatar styleClasses={[CoreClasses.BG.BG_PRIMARY]}>WR</CoreAvatar>
 
-            <CoreAvatar styleClasses={[CoreClasses.BG.BG_PRIMARY]}>W</CoreAvatar>
-
-            <CoreAvatar styleClasses={[CoreClasses.BG.BG_SECONDARY]}>WR</CoreAvatar>
+            <CoreAvatar styleClasses={[CoreClasses.BG.BG_SECONDARY]}>OP</CoreAvatar>
           </CoreStack>
         }
       />
@@ -111,9 +114,7 @@ export default function CoreAvatarDocs() {
     src="https://picsum.photos/200?random=1"
     styleClasses={[CoreClasses.DATA_DISPLAY.AVATAR_SMALL]}
   />
-  <CoreAvatar
-    src="https://picsum.photos/200?random=2"
-  />
+  <CoreAvatar src="https://picsum.photos/200?random=2"/>
   <CoreAvatar
     src="https://picsum.photos/200?random=3"
     styleClasses={[CoreClasses.DATA_DISPLAY.AVATAR_MEDIUM]}
@@ -172,7 +173,8 @@ export default function CoreAvatarDocs() {
         description={
           <>
             Icon avatars are created by passing an icon as
-            {/* eslint-disable-next-line react/jsx-newline */}
+            {" "}
+
             <CodeBlock>{" children "}</CodeBlock>.
           </>
         }
@@ -212,7 +214,7 @@ export default function CoreAvatarDocs() {
         title={"Variants"}
         description={
           <>
-            If you need square or rounded avatars, use the <CodeBlock>{"variant"}</CodeBlock> prop.
+            If you need square or rounded avatars, use the {" "}<CodeBlock>{"variant"}</CodeBlock>{" "}prop.
           </>
         }
         code={`<CoreStack styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER]} spacing={2} direction="row">
@@ -248,18 +250,20 @@ export default function CoreAvatarDocs() {
               }
             </CoreTypographyBody1>
 
-            <CoreTypographyBody1>
-              - the provided children - the first letter of the alt text - a generic avatar icon
-            </CoreTypographyBody1>
+            { /**
+            * @todo CoreList need to be fix 
+            * the provided children
+              the first letter of the alt text
+              a generic avatar icon
+             */}
           </>
         }
-        code={`
-<CoreStack styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER]} direction="row" spacing={2}>
+        code={`<CoreStack styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER]} direction="row" spacing={2}>
   <CoreAvatar
     styleClasses={[CoreClasses.BG.BG_PRIMARY]}
     alt="Remy Sharp"
     src="/broken-image.jpg"
-  >B</CoreAvatar>
+  >W</CoreAvatar>
   <CoreAvatar
     styleClasses={[CoreClasses.BG.BG_PRIMARY]}
     alt="Remy Sharp"
@@ -279,7 +283,7 @@ export default function CoreAvatarDocs() {
               alt="Remy Sharp"
               src="/broken-image.jpg"
             >
-              B
+              W
             </CoreAvatar>
 
             <CoreAvatar
@@ -295,7 +299,12 @@ export default function CoreAvatarDocs() {
 
       <CodeSample
         title="Grouped"
-        description="AvatarGroup renders its children as a stack. Use the max prop to limit the number of avatars."
+        description={
+          <>
+          AvatarGroup renders its children as a stack. Use the <CodeBlock>max</CodeBlock> prop to limit the number of avatars
+          
+          </>
+        }
         code={`<CoreStack styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER]} direction="row" spacing={2}>
   <CoreAvatarGroup max={4}>
     <CoreAvatar alt="Some Photo 1" src="https://picsum.photos/200?random=1" />
@@ -328,7 +337,11 @@ export default function CoreAvatarDocs() {
 
       <CodeSample
         title="Total avatars"
-        description="If you need to control the total number of avatars not shown, you can use the total prop."
+        description={
+          <>
+          If you need to control the total number of avatars not shown, you can use the <CodeBlock>total</CodeBlock> prop.
+          </>
+        }
         code={`<CoreStack styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER]} direction="row" spacing={2}>
   <CoreAvatarGroup total={24}>
     <CoreAvatar alt="Some Photo 1" src="https://picsum.photos/200?random=1" />
@@ -357,18 +370,26 @@ export default function CoreAvatarDocs() {
       />
 
       <CodeSample
-        title="Custom surplus (NOT IMPLEMENTED)"
-        description="Set the renderSurplus prop as a callback to customize the surplus avatar. The callback will receive the surplus number as an argument based on the children and the max prop, and should return a React.ReactNode.
-The renderSurplus prop is useful when you need to render the surplus based on the data sent from the server."
-        code={`
-          <CoreAvatarGroup 
-            renderSurplus={(surplus) => <CoreSpan>+{surplus.toString()[0]}k</>}
-            total={4250}>
+        title="Custom surplus"
+        description={
+          <>
+          Set the <CodeBlock>renderSurplus</CodeBlock> prop as a callback to customize the surplus avatar. The callback will receive the surplus number as an argument based on the children and the 
+
+            {" "}
+
+            <CodeBlock>max</CodeBlock> prop, and should return a
+
+            <CodeBlock>React.ReactNode.</CodeBlock>.
+
+The <CodeBlock>renderSurplus</CodeBlock> prop is useful when you need to render the surplus based on the data sent from the server.
+          </>
+        }
+        code={`<CoreAvatarGroup renderSurplus={(surplus) => <CoreSpan>+{surplus.toString()[0]}k</>}total={4250}>
     <CoreAvatar alt="Some Photo 1" src="https://picsum.photos/200?random=1" />
     <CoreAvatar alt="Some Photo 2" src="https://picsum.photos/200?random=2" />
     <CoreAvatar alt="Some Photo 3" src="https://picsum.photos/200?random=3" />
     <CoreAvatar alt="Some Photo 4" src="https://picsum.photos/200?random=4" />
-          </CoreAvatarGroup>
+</CoreAvatarGroup>
         `}
         renderElement={
           <CoreStack
@@ -378,7 +399,7 @@ The renderSurplus prop is useful when you need to render the surplus based on th
           >
             <CoreAvatarGroup
               renderSurplus={(surplus) => <CoreSpan>+{surplus.toString()[0]}k</CoreSpan>}
-              total={4250}
+              total={4251}
             >
               <CoreAvatar alt="Some Photo 1" src="https://picsum.photos/200?random=1" />
 
@@ -396,54 +417,7 @@ The renderSurplus prop is useful when you need to render the surplus based on th
         title="With badge (NOT IMPLEMENTED)"
         description=""
         code={`
-const StyledBadge = styled(Badge)(({ theme }) => ({
-  '& .MuiBadge-badge': {
-    backgroundColor: '#44b700',
-    color: '#44b700',
-    boxShadow: \`0 0 0 2px \${theme.palette.background.paper}\`,
-    '&::after': {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      width: '100%',
-      height: '100%',
-      borderRadius: '50%',
-      animation: 'ripple 1.2s infinite ease-in-out',
-      border: '1px solid currentColor',
-      content: '""',
-    },
-  },
-  '@keyframes ripple': {
-    '0%': {
-      transform: 'scale(.8)',
-      opacity: 1,
-    },
-    '100%': {
-      transform: 'scale(2.4)',
-      opacity: 0,
-    },
-  },
-}));
-
-<CoreStack styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER]} direction="row" spacing={2}>
-  <StyledBadge
-    overlap="circular"
-    anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-    variant="dot"
-  >
-    <CoreAvatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-  </StyledBadge>
-  <CoreBadge
-    overlap="circular"
-    anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-    badgeContent={
-      <SmallAvatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-    }
-  >
-    <CoreAvatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-  </CoreBadge>
-</CoreStack>
-        `}
+          `}
         renderElement={
           <CoreStack
             styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER]}
