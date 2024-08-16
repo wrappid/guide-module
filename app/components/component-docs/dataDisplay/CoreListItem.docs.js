846713bda00611f2516c61624619f82261692d0d
arkaprota <arkaprota.ghosh@gmail.com>
2024-08-16 13:47:45 +0530

    fix(core): :bug: error showing in CoreListItem
    there is a error in corelistitems and there was no description given. The bug has been fixed and the description is also given properly.
    
    Ref: #183

--- app/components/component-docs/dataDisplay/CoreListItem.docs.js
+++ app/components/component-docs/dataDisplay/CoreListItem.docs.js
@@ -9,115 +9,178 @@ import {
   CoreListItemText,
   CoreListItemIcon,
   CoreListItemButton,
-  CoreDivider
+  CoreDivider,
+  CoreH5,
+  CoreStack,
+  CorePaper
 } from "@wrappid/core";
 
+import CodeImport from "../../CodeImport";
 import CodeSample from "../../CodeSample";
 import ComponentProps from "../../ComponentProps";
 
 export default function CoreListItemDocs() {
   return (
     <>
-      <CoreH4>{"CoreListItem "}</CoreH4>
+      <CoreH4>{"CoreListItem"}</CoreH4>
 
-      <CoreTypographyBody1>
-        COMPONENT_DESCRIPTION
+      <CoreTypographyBody1 styleClasses={[CoreClasses.PADDING.PT2]}>
+        In Wrappid Component, the CoreListItem component is used to represent a single item in a list. Wrappid provides a rich set of components that make it easy to build lists with various levels of customization and functionality. Here is a breakdown of the key aspects of ListItem:
       </CoreTypographyBody1>
 
+      <CodeImport name="CoreListItem" />
+      
       <CodeSample
         title={"Basic CoreListItem"}
-        description={"DESCRIPTION_OF_THE_SAMPLE"}
+        description={<>
+          <CoreTypographyBody1>
+            CoreListItem Component: This is the core component used to create an item in a list. It is typically wrapped inside a List component.
+          </CoreTypographyBody1>
+
+          <CoreH5>Subcomponents</CoreH5>
+
+          <CoreBox styleClasses={[CoreClasses.MARGIN.SMML1]}>
+            <CoreList variant="HTML" listType="DISC">
+              <CoreListItem>
+                <CoreListItemText primary="CoreListItemText: Used to display the primary and secondary text within a list item. You can pass primary and secondary props to this component to display different levels of text."/>
+              </CoreListItem>
+
+              <CoreListItem>
+                <CoreListItemText primary="CoreListItemButton: It is designed to be used when you want a list item to have the look and feel of a button. It provides a more semantic and accessible way to create clickable list items."/>
+              </CoreListItem>
+
+              <CoreListItem>
+                <CoreListItemText primary="CoreListItemIcon: Allows you to add an icon to the left side of the list item. This is commonly used with icons from the Material-UI icon library." />
+              </CoreListItem>
+
+              <CoreListItem>
+                <CoreListItemText primary="CoreListItemAvatar: Used to display an avatar on the left side of the list item, which is useful for lists with user profiles or similar content." />
+              </CoreListItem>
+
+              <CoreListItem>
+                <CoreListItemText primary="CoreListItemSecondaryAction: Allows you to add an action element (like a button or icon) to the right side of the list item." />
+              </CoreListItem>
+            </CoreList>
+          </CoreBox>
+        </>}
         code={`<CoreBox
-styleClasses={[
-  CoreClasses.WIDTH.W_100.MAX_W_100,
-  CoreClasses.BORDER.BORDER_2,
-]}
+  styleClasses={[
+    CoreClasses.WIDTH.W_100,
+    CoreClasses.MARGIN.M4,
+    CoreClasses.DISPLAY.FLEX,
+    CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER,
+    CoreClasses.BORDER.BORDER_2
+  ]}
 >
+  <CoreStack
+    component={CorePaper}
+    styleClasses={[CoreClasses.WIDTH.W_25]}
+  >
+    {/* <nav aria-label="main mailbox folders"> */}
     <CoreList>
-        <CoreListItem disablePadding>
-          <CoreListItemButton>
-            <CoreListItemIcon>
-              <CoreIcon icon="inbox" />
-            </CoreListItemIcon>
-            <CoreListItemText primary="Inbox" />
-          </CoreListItemButton>
-        </CoreListItem>
-        <CoreListItem disablePadding>
-          <CoreListItemButton>
-            <CoreListItemIcon>
-              <CoreIcon icon="drafts" />
-            </CoreListItemIcon>
-            <CoreListItemText primary="Drafts" />
-          </CoreListItemButton>
-        </CoreListItem>
+      <CoreListItem disablePadding>
+        <CoreListItemButton>
+          <CoreListItemIcon>
+            <CoreIcon icon="inbox" />
+          </CoreListItemIcon>
+
+          <CoreListItemText primary="Inbox" />
+        </CoreListItemButton>
+      </CoreListItem>
+
+      <CoreListItem disablePadding>
+        <CoreListItemButton>
+          <CoreListItemIcon>
+            <CoreIcon icon="drafts" />
+          </CoreListItemIcon>
+
+          <CoreListItemText primary="Drafts" />
+        </CoreListItemButton>
+      </CoreListItem>
     </CoreList>
+
     <CoreDivider />
+
     <CoreList>
-        <CoreListItem disablePadding>
-          <CoreListItemButton>
-            <CoreListItemText primary="Trash" />
-          </CoreListItemButton>
-        </CoreListItem>
-        <CoreListItem disablePadding>
-          <CoreListItemButton component="a" href="#simple-Corelist">
-            <CoreListItemText primary="Spam" />
-          </CoreListItemButton>
-        </CoreListItem>
+      <CoreListItem disablePadding>
+        <CoreListItemButton>
+          <CoreListItemText primary="Trash" />
+        </CoreListItemButton>
+      </CoreListItem>
+
+      <CoreListItem disablePadding>
+        <CoreListItemButton component="a" href="#simple-Corelist">
+          <CoreListItemText primary="Spam" />
+        </CoreListItemButton>
+      </CoreListItem>
     </CoreList>
-</CoreBox>    `}
+
+  </CoreStack>
+</CoreBox>`}
         renderElement={
           <>
             <CoreBox
-              styleClasses={[CoreClasses.WIDTH.W_100.MAX_W_100, CoreClasses.BORDER.BORDER_2]}
+              styleClasses={[
+                CoreClasses.WIDTH.W_100,
+                CoreClasses.MARGIN.M4,
+                CoreClasses.DISPLAY.FLEX,
+                CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER,
+                CoreClasses.BORDER.BORDER_2
+              ]}
               // sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
             >
-              {/* <nav aria-label="main mailbox folders"> */}
-              <CoreList>
-                <CoreListItem disablePadding>
-                  <CoreListItemButton>
-                    <CoreListItemIcon>
-                      <CoreIcon icon="inbox" />
-                    </CoreListItemIcon>
+              <CoreStack
+                component={CorePaper}
+                styleClasses={[CoreClasses.WIDTH.W_25]}
+              >
+                {/* <nav aria-label="main mailbox folders"> */}
+                <CoreList>
+                  <CoreListItem disablePadding>
+                    <CoreListItemButton>
+                      <CoreListItemIcon>
+                        <CoreIcon icon="inbox" />
+                      </CoreListItemIcon>
 
-                    <CoreListItemText primary="Inbox" />
-                  </CoreListItemButton>
-                </CoreListItem>
+                      <CoreListItemText primary="Inbox" />
+                    </CoreListItemButton>
+                  </CoreListItem>
 
-                <CoreListItem disablePadding>
-                  <CoreListItemButton>
-                    <CoreListItemIcon>
-                      <CoreIcon icon="drafts" />
-                    </CoreListItemIcon>
+                  <CoreListItem disablePadding>
+                    <CoreListItemButton>
+                      <CoreListItemIcon>
+                        <CoreIcon icon="drafts" />
+                      </CoreListItemIcon>
 
-                    <CoreListItemText primary="Drafts" />
-                  </CoreListItemButton>
-                </CoreListItem>
-              </CoreList>
+                      <CoreListItemText primary="Drafts" />
+                    </CoreListItemButton>
+                  </CoreListItem>
+                </CoreList>
 
-              <CoreDivider />
+                <CoreDivider />
 
-              {/* <nav aria-label="secondary mailbox folders"> */}
-              <CoreList>
-                <CoreListItem disablePadding>
-                  <CoreListItemButton>
-                    <CoreListItemText primary="Trash" />
-                  </CoreListItemButton>
-                </CoreListItem>
+                {/* <nav aria-label="secondary mailbox folders"> */}
+                <CoreList>
+                  <CoreListItem disablePadding>
+                    <CoreListItemButton>
+                      <CoreListItemText primary="Trash" />
+                    </CoreListItemButton>
+                  </CoreListItem>
 
-                <CoreListItem disablePadding>
-                  <CoreListItemButton component="a" href="#simple-Corelist">
-                    <CoreListItemText primary="Spam" />
-                  </CoreListItemButton>
-                </CoreListItem>
-              </CoreList>
+                  <CoreListItem disablePadding>
+                    <CoreListItemButton component="a" href="#simple-Corelist">
+                      <CoreListItemText primary="Spam" />
+                    </CoreListItemButton>
+                  </CoreListItem>
+                </CoreList>
 
-              {/* </nav> */}
+                {/* </nav> */}
+              </CoreStack>
             </CoreBox>
           </>
         }
       />
 
-      <ComponentProps component={CoreListItem} />
+      <ComponentProps component={[CoreListItem]} />
     </>
   );
 }
