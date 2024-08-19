2534ebc09b98bd8c60d30af5396f7e43b10d8672
PritamIT2023 <pritammondal8981@gmail.com>
2024-08-19 17:19:27 +0530

    docs(global): :memo: docs update CoreCircularProgress
    docs update CoreCircularProgress
    
    Ref: #201

--- app/components/component-docs/feedback/CoreCircularProgress.docs.js
+++ app/components/component-docs/feedback/CoreCircularProgress.docs.js
@@ -2,31 +2,85 @@ import React from "react";
 
 import {
   CoreH4,
-  CoreAlert,
   CoreStack,
-  CoreCircularProgress
+  CoreCircularProgress,
+  CoreTypographyBody1,
+  CoreBox,
+  CoreClasses
   // CoreCircularProgressWithLabel
 } from "@wrappid/core";
 
+import CodeBlock from "../../CodeBlock";
+import CodeImport from "../../CodeImport";
 import CodeSample from "../../CodeSample";
 import ComponentProps from "../../ComponentProps";
 
+function CircularProgressWithLabel(props) {
+  return (
+    <CoreBox 
+      styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER]}
+    // styleClasses={[CoreClasses.DISPLAY.INLINE_FLEX, CoreClasses.POSITION.POSITION_RELATIVE]} /* sx={{ display: "inline-flex", position: "relative" }} */
+    >
+      <CoreCircularProgress styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER]} variant="determinate" {...props} />
+
+      <CoreBox
+      >
+        <CoreTypographyBody1 variant="caption">
+          {`${Math.round(props.value)}%`}
+        </CoreTypographyBody1>
+      </CoreBox>
+    </CoreBox>
+  );
+}
+
 export default function CoreCircularProgressDocs() {
   // eslint-disable-next-line no-unused-vars
-  const [progress, setProgress] = React.useState(0);
+  const [progress, setProgress] = React.useState(10);
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((prevProgress) => (prevProgress >= 100 ? 0 : prevProgress + 10));
+    }, 800);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
 
   return (
     <>
       <CoreH4>CoreCircularProgress</CoreH4>
 
+      <CoreTypographyBody1>
+          CoreCircularProgress indicators inform users about the status of ongoing processes, such as loading an app, submitting a form, or saving updates.
+      </CoreTypographyBody1>
+
+      <CoreTypographyBody1>
+          1.Determinate indicators display how long an operation will take.
+      </CoreTypographyBody1>
+
+      <CoreTypographyBody1>
+          2.Indeterminate indicators visualize an unspecified wait time.
+      </CoreTypographyBody1>
+
+      <CodeImport name="CoreCircularProgress" />
+
       <CodeSample
         title={"Circular indeterminate"}
+        description={<>
+          <CoreTypographyBody1>Indeterminate indicators visualize an unspecified wait time</CoreTypographyBody1>
+        </>}
         code={"<CoreCircularProgress />"}
         renderElement={<CoreCircularProgress />}
       />
 
       <CodeSample
         title={"Circular Color"}
+        description={<>
+          <CoreTypographyBody1>
+             CoreCircularProgress with <CodeBlock>color</CodeBlock> props.
+          </CoreTypographyBody1>
+        </>}
         code={`<CoreCircularProgress color="secondary" />
 <CoreCircularProgress color="success" />
 <CoreCircularProgress color="inherit" />`}
@@ -43,6 +97,11 @@ export default function CoreCircularProgressDocs() {
 
       <CodeSample
         title={"Circular determinate"}
+        description={<>
+          <CoreTypographyBody1>
+        Determinate indicators display how long an operation will take.
+          </CoreTypographyBody1>
+        </>}
         code={`<CoreCircularProgress variant="determinate" value={25} />
 <CoreCircularProgress variant="determinate" value={50} />
 <CoreCircularProgress variant="determinate" value={75} />
@@ -63,21 +122,35 @@ export default function CoreCircularProgressDocs() {
         }
       />
 
-      <CoreAlert severity="warning">
-        Interactive integration Info: TODO
-      </CoreAlert>
-
-      {/**@todo Interactive integration*/}
-      <CodeSample title={"Interactive integration(TODO)"} />
-
-      <CoreAlert severity="error">
-        Circular with label Error: CoreCircularProgressWithLabel is Not
-        Implemented
-      </CoreAlert>
+      <CodeSample
+        title={"Interactive integration(TODO)"}
+        description={<>
+          <CoreTypographyBody1>
+          </CoreTypographyBody1>
+        </>}
+        code={" "}
+        renderElement={
+          <>
+         
+          </>
+        }
+      />
 
       <CodeSample
-        title={"Circular with label(NOT IMPLEMENTED)"}
+        title={"Circular with label"}
+        description={
+          <>
+            <CoreTypographyBody1>
+            CoreCircularProgress Label indicate users about the status of ongoing processes, such as loading an app, submitting a form, or saving updates.
+            </CoreTypographyBody1>
+          </>
+        }
         code={"<CoreCircularProgressWithLabel value={progress} />"}
+        renderElement={
+          <>
+            <CircularProgressWithLabel value={progress} />;
+          </>
+        }
       />
 
       <ComponentProps component={CoreCircularProgress} />
