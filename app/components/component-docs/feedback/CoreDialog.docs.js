df894abe5ee4f059d084891e5f26f799e59a77ed
Pritam Bag <130136389+PritamBag@users.noreply.github.com>
2024-08-21 18:27:41 +0530

    docs(global): :memo: basic updation
    here I try to update the documentation
    
    Ref: #210

--- app/components/component-docs/feedback/CoreDialog.docs.js
+++ app/components/component-docs/feedback/CoreDialog.docs.js
@@ -1,67 +1,142 @@
 import React from "react";
 
+// eslint-disable-next-line import/no-unresolved
+import Slide from "@mui/material/Slide";
 import {
   CoreH4,
   CoreTypographyBody1,
+  CoreButton,
+  CoreList,
+  CoreListItem,
+  CoreListItemButton,
+  CoreListItemAvatar,
+  CoreAvatar,
+  CoreListItemText,
+  CoreBox,
   CoreDialog,
-  CoreStack
+  CoreDialogContext
   // CoreSimpleDialog
   // CoreDialogTitle
 } from "@wrappid/core";
 
-import CodeImport from "../../CodeImport";
 import CodeSample from "../../CodeSample";
-import ComponentProps from "../../ComponentProps";
+import ComponentDocs from "../ComponentDocs";
+
+const emails = ["username@gmail.com", "userPritam@gmail.com"];
+
+function CoreSimpleDialog(props) {
+  const { onClose, selectedValue, open } = props;
+
+  const handleClose = () => {
+    onClose(selectedValue);
+  };
+
+  const handleListItemClick = (value) => {
+    onClose(value);
+  };
+
+  return (
+    
+    <CoreBox onClose={handleClose} open={open}>
+      <CoreH4>Set backup account</CoreH4>
+
+      <CoreList>
+        {emails.map((email) => (
+          <CoreListItem disablePadding key={email}>
+            <CoreListItemButton onClick={() => handleListItemClick(email)}>
+              <CoreListItemAvatar>
+                <CoreAvatar src="/broken-image.jpg" />
+              </CoreListItemAvatar>
+
+              <CoreListItemText primary={email} />
+            </CoreListItemButton>
+          </CoreListItem>
+        ))}
+
+        <CoreListItem disablePadding>
+          <CoreListItemButton autoFocus onclick={() => handleListItemClick("addAccount")}>
+            <CoreListItemAvatar>
+              <CoreAvatar src="https://placehold.co/400x400?text=%2B" />
+            </CoreListItemAvatar>
+
+            <CoreListItemText primary="Add account" />
+          </CoreListItemButton>
+        </CoreListItem>
+      </CoreList>
+    </CoreBox>
+  );
+}
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="down" ref={ref} {...props} />;
+});
 
 export default function CoreDialogDocs() {
   // eslint-disable-next-line no-unused-vars
   const [open, setOpen] = React.useState(false);
   // eslint-disable-next-line no-unused-vars
-  const [selectedValue, setSelectedValue] = React.useState("abc");
+  const [selectedValue, setSelectedValue] = React.useState(emails[1]);
+
+  const { setDialog } = React.useContext(CoreDialogContext);
+  
+  React.useEffect(() => {
+    
+    setDialog({
+      dialogProps: { TransitionComponent: Transition },
+      showDialog : true,
+      subtitle   : "Password changed successfully.",
+      title      : "Success"
+    });
+  }, [open]);
+
+  const handleClickOpen = () => {
+    // eslint-disable-next-line no-console
+    console.log("Hello");
+    setOpen(!open);
+  };
+
+  const handleClose = (value) => {
+    setOpen(false);
+    setSelectedValue(value);
+  };
 
   return (
     <>
-      <CoreDialog severity="warning">
-        CoreDialog in not implemented in mobile
-      </CoreDialog>
+      <ComponentDocs
+        component={CoreDialog}
+        description="CoreDialogs inform users about a task and can contain critical information, require decisions, or involve multiple tasks. 
+        A Dialog is a type of modal window that appears in front of app content to provide critical information or ask for a decision. Dialogs disable all app functionality when they appear, and remain on screen until confirmed, dismissed, or a required action has been taken.
+        Dialogs are purposefully interruptive, so they should be used sparingly."
+        samples={
+          <>
+            <CodeSample
+              title={"Introduction"}
+              description={"Dialogs are implemented using a collection of related components:Dialog: the parent component that renders the modal.Dialog Title: a wrapper used for the title of a Dialog.Dialog Actions: an optional container for a Dialog's Buttons.Dialog Content: an optional container for displaying the Dialog's content.Dialog Content Text: a wrapper for text inside of <DialogContent />.Slide: optional Transition used to slide the Dialog in from the edge of the screen."}
+              renderElement={
+                <>
+                  <CoreBox>
 
-      <CoreH4>CoreDialog</CoreH4>
+                    <CoreTypographyBody1>
+                    Selected: {selectedValue}
+                    </CoreTypographyBody1>
 
-      <CoreStack direction="column" spacing={2}>
-        <CoreTypographyBody1>
-          Dialogs inform users about a task and can contain critical
-          information, require decisions, or involve multiple tasks.
-        </CoreTypographyBody1>
+                    <br />
 
-        <CoreTypographyBody1>
-          A Dialog is a type of modal window that appears in front of app
-          content to provide critical information or ask for a decision. Dialogs
-          disable all app functionality when they appear, and remain on screen
-          until confirmed, dismissed, or a required action has been taken.
-        </CoreTypographyBody1>
+                    <CoreButton variant="outlined" onClick={handleClickOpen}>
+                    Open simple dialog
+                    </CoreButton>
 
-        <CoreTypographyBody1>
-          Dialogs are purposefully interruptive, so they should be used
-          sparingly.
-        </CoreTypographyBody1>
-      </CoreStack>
-
-      <CodeImport name="CoreDialog" />
-
-      <CodeSample
-        title={"Basic dialog"}
-        description={`
-Simple dialogs can provide additional details or actions about a list item. For example, they can display avatars, icons, clarifying subtext, or orthogonal actions (such as adding an account).
-
-Touch mechanics:
-
-Choosing an option immediately commits the option and closes the menu
-Touching outside of the dialog, or pressing Back, cancels the action and closes the dialog
- `}
-        renderElement={<></>}
+                    <CoreSimpleDialog
+                      selectedValue={selectedValue}
+                      open={open}
+                      onClose={handleClose}
+                    />
+                  </CoreBox>
+                </>
+              }
+            />
+          </>
+        }
       />
-
-      <ComponentProps component={CoreDialog} />
     </>
   );
 }
