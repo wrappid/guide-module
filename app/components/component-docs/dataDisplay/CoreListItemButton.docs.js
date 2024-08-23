26fad904361bbc21390c2b17ff3f8a15b626e838
arkaprota <arkaprota.ghosh@gmail.com>
2024-08-23 12:31:29 +0530

    docs(global): :memo: corelistitembutton is documentation
    the documentation has been updated according to the new format
    
    Ref: #191

--- app/components/component-docs/dataDisplay/CoreListItemButton.docs.js
+++ app/components/component-docs/dataDisplay/CoreListItemButton.docs.js
@@ -1,6 +1,4 @@
 import {
-  CoreH4,
-  CoreTypographyBody1,
   CoreListItemButton,
   CoreDivider,
   CoreListItemIcon,
@@ -10,87 +8,63 @@ import {
   CoreListItemText,
   CoreStack,
   CorePaper,
-  CoreClasses,
-  CoreBox
+  CoreClasses
 } from "@wrappid/core";
 
-import CodeImport from "../../CodeImport";
 import CodeSample from "../../CodeSample";
-import ComponentProps from "../../ComponentProps";
+import ComponentDocs from "../ComponentDocs";
 
 export default function CoreListItemButtonDocs() {
   return (
     <>
-      <CoreH4>{"CoreListItemButton"}</CoreH4>
+      <ComponentDocs
+        component={CoreListItemButton}
+        description="CoreListItemButton is a simplified version of CoreListItem optimized for items that act like buttons. It's designed to make it easier to create clickable CoreList items without needing to configure additional props."
+        samples={
+          <>
+            <CodeSample
+              title={"Basic CoreListItemButton"}
+              description="CoreListItemButton is the primary component used to make the CoreList item behave like a CoreButton. It automatically has button-like behavior, meaning it’s clickable and can be styled as a button. Adds an icon to the left side of the CoreListItemButton, typically used to represent the content or action associated with the item."
+              code={`<CoreListItemButton>
+  <CoreListItemIcon>
+    <CoreIconText icon="mail" />
+  </CoreListItemIcon>
 
-      <CoreTypographyBody1 styleClasses={[CoreClasses.PADDING.PT2]}>
-        CoreListItemButton is a component in Wrappid that is a simplified version of CoreListItem optimized for items that act like buttons. {"It's"} designed to make it easier to create clickable CoreList items without needing to configure additional props.
-      </CoreTypographyBody1>
+  <CoreListItemText primary="Inbox" />
+</CoreListItemButton>`}
+              expandedCode={`import {
+  BlankLayout,
+  CoreBox,
+  CoreClasses,
+  CoreListItemButton,
+  CoreListItemIcon,
+  CoreIconText,
+  CoreLayoutItem,
+  CorePaper,
+  CoreStack,
+  CoreDivider,
+  CoreList,
+  CoreListItem,
+  CoreListItemText
+} from "@wrappid/core";
 
-      <CodeImport name="CoreListItemButton" />
+export default function BasicCoreListItem() {
+  return (
+    <>
       
-      <CodeSample
-        title={"Basic Structure"}
-        description={<>
-          <CoreBox styleClasses={[CoreClasses.PADDING.SMPL2]}>
-            <CoreList variant="HTML" listType="DISC">
-              <CoreListItem>
-                <CoreListItemText primary="CoreListItemButton: This is the primary component used to make the CoreList item behave like a CoreButton. It automatically has button-like behavior, meaning it’s clickable and can be styled as a button." />
-              </CoreListItem>
+      <CoreLayoutItem
+        id={BlankLayout.PLACEHOLDER.CONTENT}
+        styleClasses={[CoreClasses.FLEX.FLEX, CoreClasses.FLEX.DIRECTION_COLUMN, CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER, CoreClasses.PADDING.P5]}>
 
-              <CoreListItem>
-                <CoreListItemText primary="CoreListItemIcon: Adds an icon to the left side of the CoreListItemButton, typically used to represent the content or action associated with the item."/>
-              </CoreListItem>
-
-              <CoreListItem>
-                <CoreListItemText primary="CoreListItemText: Displays the text content of the CoreList item, such as labels or descriptions."/>
-              </CoreListItem>
-
-              <CoreListItem>
-                <CoreListItemText primary="CoreDivider: Adds a line between items to visually separate them."/>
-              </CoreListItem>
-            </CoreList>
-          </CoreBox>
-        </>}
-        code={`<CoreStack
-  component={CorePaper}
-  styleClasses={[CoreClasses.WIDTH.W_25]}
->
-  <CoreList component="nav">
-    <CoreListItemButton>
-      <CoreListItemIcon>
-        <CoreIconText icon="mail" />
-      </CoreListItemIcon>
-      <CoreListItemText primary="Inbox" />
-    </CoreListItemButton>
-
-    <CoreListItemButton>
-      <CoreListItemIcon>
-        <CoreIconText icon="drafts"/>
-      </CoreListItemIcon>
-      <CoreListItemText primary="Draft" />
-    </CoreListItemButton>
-
-    <CoreDivider />
-  
-    <CoreListItem disablePadding> 
-      <CoreListItemButton> 
-        <CoreListItemText  
-          primary="Javascript" /> 
-      </CoreListItemButton> 
-    </CoreListItem> 
-
-    <CoreListItem disablePadding> 
-      <CoreListItemButton
-        component="a"
-        href="#simple-list"> 
-        <CoreListItemText  
-          primary="PHP" /> 
-      </CoreListItemButton> 
-    </CoreListItem> 
-  </CoreList>
-</CoreStack>`}
-        renderElement={<>
+        <CoreBox
+          styleClasses={[
+            CoreClasses.WIDTH.W_100,
+            CoreClasses.MARGIN.M4,
+            CoreClasses.DISPLAY.FLEX,
+            CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER,
+            CoreClasses.BORDER.BORDER_2
+          ]}
+        >
           <CoreStack
             component={CorePaper}
             styleClasses={[CoreClasses.WIDTH.W_25]}
@@ -131,10 +105,58 @@ export default function CoreListItemButtonDocs() {
               </CoreListItem> 
             </CoreList>
           </CoreStack>
-        </>}
-      />
+        </CoreBox>
+      </CoreLayoutItem>
+    </>
+  );
+};`}
+              renderElement={<>
+                <CoreStack
+                  component={CorePaper}
+                  styleClasses={[CoreClasses.WIDTH.W_25]}
+                >
+                  <CoreList component="nav">
+                    <CoreListItemButton>
+                      <CoreListItemIcon>
+                        <CoreIconText icon="mail" />
+                      </CoreListItemIcon>
 
-      <ComponentProps component={CoreListItemButton} />
+                      <CoreListItemText primary="Inbox" />
+                    </CoreListItemButton>
+
+                    <CoreListItemButton>
+                      <CoreListItemIcon>
+                        <CoreIconText icon="drafts"/>
+                      </CoreListItemIcon>
+
+                      <CoreListItemText primary="Draft" />
+                    </CoreListItemButton>
+
+                    <CoreDivider />
+            
+                    <CoreListItem disablePadding> 
+                      <CoreListItemButton> 
+                        <CoreListItemText  
+                          primary="Inbox" /> 
+                      </CoreListItemButton> 
+                    </CoreListItem> 
+
+                    <CoreListItem disablePadding> 
+                      <CoreListItemButton
+                        component="a"
+                        href="#"> 
+                        <CoreListItemText  
+                          primary="Draft" /> 
+                      </CoreListItemButton> 
+                    </CoreListItem> 
+                  </CoreList>
+                </CoreStack>
+              </>
+              }
+            />
+          </>
+        }
+      />
     </>
   );
 }
