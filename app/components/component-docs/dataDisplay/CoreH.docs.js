c0e667336cfb84590630073264793186560ad037
PritamIT2023 <pritammondal8981@gmail.com>
2024-08-23 12:29:55 +0530

    docs(global): :memo: update documentation CoreH
    update documentation CoreH
    
    Ref: #176

--- app/components/component-docs/dataDisplay/CoreH.docs.js
+++ app/components/component-docs/dataDisplay/CoreH.docs.js
@@ -1,24 +1,81 @@
 import {
-  CoreH4,
-  CoreTypographyBody1
+  CoreBox,
+  CoreH, CoreH1, CoreH2, CoreH3, CoreH4, CoreH5, CoreH6 
 } from "@wrappid/core";
   
 import CodeSample from "../../CodeSample";
+import ComponentDocs from "../ComponentDocs";
 
 export default function CoreHDocs() {
   return (
     <>
-      <CoreH4>CoreH</CoreH4>
+      <ComponentDocs
+        component={CoreH}
+        description="Please provide your component description to describe what is it used for."
+        samples={
+          <>
+            <CodeSample
+              title={"Headings"}
+              description={"Example of different type of heading."}
+              expandedCode={`import {
+  CoreBox,
+  CoreH1,
+  CoreH2,
+  CoreH3,
+  CoreH4,
+  CoreH5,
+  CoreH6 
+} from "@wrappid/core";
   
-      <CoreTypographyBody1>
-          COMPONENT_DESCRIPTION
-      </CoreTypographyBody1>
-  
-      <CodeSample
-        title={"TITLE_OF_THE_SAMPLE (NOT_DEFINED)"}
-        description={"DESCRIPTION_OF_THE_SAMPLE"}
-        code={"PRE-FORMATTED_CODE_GOES_HERE"}
-        renderElement={<></>}
+export default function CoreHTypes() {
+  return (
+    <CoreBox>
+      <CoreH1 gutterBottom>h1.heading</CoreH1>
+
+      <CoreH2 gutterBottom>h2.heading</CoreH2>
+
+      <CoreH3 gutterBottom>h3.heading</CoreH3>
+
+      <CoreH4 gutterBottom>h4.heading</CoreH4>
+
+      <CoreH5 gutterBottom>h5.heading</CoreH5>
+
+      <CoreH6 gutterBottom>h6.heading</CoreH6>
+    </CoreBox>           
+  );
+}`}
+              code={`<CoreBox>
+  <CoreH1 gutterBottom>h1.heading</CoreH1>
+
+  <CoreH2 gutterBottom>h2.heading</CoreH2>
+
+  <CoreH3 gutterBottom>h3.heading</CoreH3>
+
+  <CoreH4 gutterBottom>h4.heading</CoreH4>
+
+  <CoreH5 gutterBottom>h5.heading</CoreH5>
+
+  <CoreH6 gutterBottom>h6.heading</CoreH6>
+</CoreBox>`}
+              renderElement={<>
+                <CoreBox>
+
+                  <CoreH1 gutterBottom>h1.heading</CoreH1>
+
+                  <CoreH2 gutterBottom>h2.heading</CoreH2>
+
+                  <CoreH3 gutterBottom>h3.heading</CoreH3>
+
+                  <CoreH4 gutterBottom>h4.heading</CoreH4>
+
+                  <CoreH5 gutterBottom>h5.heading</CoreH5>
+
+                  <CoreH6 gutterBottom>h6.heading</CoreH6>
+                </CoreBox>
+              </>}
+            />
+          </>
+        }
       />
         
       {/* eslint-disable-next-line etc/no-commented-out-code */}
