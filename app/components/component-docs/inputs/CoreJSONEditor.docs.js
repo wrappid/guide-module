41582f1a6f912b2d5e05280297f8fc5c235885bf
anantakumarghosh <antaghosh@gmail.com>
2024-09-09 17:31:35 +0530

    docs(core): :memo: add CoreJSON related docs
    add CoreJSON related docs
    
    ref: #305

--- app/components/component-docs/inputs/CoreJSONEditor.docs.js
+++ app/components/component-docs/inputs/CoreJSONEditor.docs.js
@@ -1,34 +1,19 @@
-import {
-  CoreH4,
-  CoreJSONEditor,
-  CoreTypographyBody1
-} from "@wrappid/core";
-  
-import CodeSample from "../../CodeSample";
+import { CoreJSONEditor } from "@wrappid/core";
+
+import ComponentDocs from "../ComponentDocs";
+
+const CORE_COMPONENT = CoreJSONEditor;
+const CORE_COMPONENT_NAME = CORE_COMPONENT?.name;
 
 export default function CoreJSONEditorDocs() {
   return (
     <>
-      <CoreH4>CoreJSONEditor</CoreH4>
-  
-      <CoreTypographyBody1>
-          COMPONENT_DESCRIPTION
-      </CoreTypographyBody1>
-  
-      <CodeSample
-        title={"Simple CoreJSONEditor (NOT_WORKING)"}
-        description={"DESCRIPTION_OF_THE_SAMPLE"}
-        code={`
-<CoreJSONEditor></CoreJSONEditor>
-        `}
-        renderElement={<>
-          <CoreJSONEditor label="Json Editor"></CoreJSONEditor>
-        </>}
+      <ComponentDocs
+        component={CORE_COMPONENT}
+        description={`The ${CORE_COMPONENT_NAME} component is made for JSON field inputs.`}
+        basicSample={true}
+        basicSampleProps={{ label: "Json Editor" }}
       />
-        
-      {/* eslint-disable-next-line etc/no-commented-out-code */}
-      {/* <ComponentProps component={CoreJSONEditor} /> */}
-      
     </>
   );
 }
