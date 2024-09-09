2012362e342790378ac3c0019f04789d7297be4d
Pritam Bag <130136389+PritamBag@users.noreply.github.com>
2024-09-09 17:42:42 +0530

    docs(global): :memo: update CoreCardContent documentation
    documentation of CoreCardContent is not complete, So I update these documentation
    
    Ref: #296

--- app/components/component-docs/surfaces/CoreCardContent.docs.js
+++ app/components/component-docs/surfaces/CoreCardContent.docs.js
@@ -1,99 +1,68 @@
-import {
-  CoreBox,
-  CoreButton,
-  CoreCard,
-  CoreCardActions,
-  CoreCardContent,
-  CoreClasses,
-  CoreH4,
-  CoreTypographyBody1,
-  CoreTypographyBody2
-} from "@wrappid/core";
-  
+import { CoreCard, CoreCardContent, CoreClasses, CoreH6, CoreImage } from "@wrappid/core";
+
 import CodeSample from "../../CodeSample";
+import ComponentDocs from "../ComponentDocs";
 
 export default function CoreCardContentDocs() {
-  const bull = (
-    <CoreBox
-      component="span"
-      // sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
-    >
-      •
-    </CoreBox>
-  );
-
+  
   return (
     <>
-      <CoreH4>CoreCardContent</CoreH4>
+      <ComponentDocs 
+        component={CoreCardContent}
+        description="CoreCardContent is a component that holds the main content of a card, such as text, images, or other elements. It provides a structured area within a card for displaying primary information."
+        samples={
+          <>
+            <CodeSample 
+              title={" "}
+              description={"Here is a basic example of how we can use CoreCardContent to enhance the card design by structuring and displaying its main content clearly and effectively."}
+              code={`<CoreCardContent styleClasses={[CoreClasses.DISPLAY.FLEX, CoreClasses.FLEX.DIRECTION_COLUMN, CoreClasses.ALIGNMENT.ALIGN_ITEMS_CENTER, CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER]}>
+  <CoreImage
+    width={120}
+    height={40} 
+    alt="Phoenicopterus ruber"
+    src="https://i0.wp.com/wrappid.dev/wp-content/uploads/2024/02/logo-1200x400-1.png?resize=1024%2C341&ssl=1" 
+  />
+
+  <CoreH6 styleClasses={[CoreClasses.PADDING.PR2]}>I&apos;m also a Wrappiders</CoreH6>
+</CoreCardContent>`}
+              expandedCode={`import { CoreCard, CoreCardContent, CoreClasses, CoreH6, CoreImage } from "@wrappid/core";
+
+export default function CoreCardDocs() {
   
-      <CoreTypographyBody1>
-          COMPONENT_DESCRIPTION
-      </CoreTypographyBody1>
-      
-      <CodeSample
-        title={"Basic CoreCardContent"}
-        description={"Although cards can support multiple actions, UI controls, and an overflow menu, use restraint and remember that cards are entry points to more complex and detailed information."}
-        code={`
-<CoreCard >
-    <CoreCardContent>
-      <CoreTypographyBody1 color="text.secondary" gutterBottom>
-         Word of the Day
-      </CoreTypographyBody1>
+  return ( 
+    <CoreCard styleClasses={[CoreClasses.WIDTH.VW_25]}>
+      <CoreCardContent styleClasses={[CoreClasses.DISPLAY.FLEX, CoreClasses.FLEX.DIRECTION_COLUMN, CoreClasses.ALIGNMENT.ALIGN_ITEMS_CENTER, CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER]}>
+        <CoreImage
+          width={120}
+          height={40} 
+          alt="Phoenicopterus ruber"
+          src="https://i0.wp.com/wrappid.dev/wp-content/uploads/2024/02/logo-1200x400-1.png?resize=1024%2C341&ssl=1" 
+        />
 
-      <CoreTypographyBody1 variant="h5" component="div">
-        be{bull}nev{bull}o{bull}lent
-      </CoreTypographyBody1>
+        <CoreH6 styleClasses={[CoreClasses.PADDING.PR2]}>I&apos;m also a Wrappiders</CoreH6>
+      </CoreCardContent>
+    </CoreCard>
+  );  
+}
+`}
+              renderElement={
+                <CoreCard styleClasses={[CoreClasses.WIDTH.VW_25]}>
+                  <CoreCardContent styleClasses={[CoreClasses.DISPLAY.FLEX, CoreClasses.FLEX.DIRECTION_COLUMN, CoreClasses.ALIGNMENT.ALIGN_ITEMS_CENTER, CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER]}>
+                    <CoreImage
+                      width={120}
+                      height={40} 
+                      alt="Phoenicopterus ruber"
+                      src="https://i0.wp.com/wrappid.dev/wp-content/uploads/2024/02/logo-1200x400-1.png?resize=1024%2C341&ssl=1" 
+                    />
 
-      <CoreTypographyBody1 color="text.secondary">
-         adjective
-      </CoreTypographyBody1>
-
-      <CoreTypographyBody2 >
-        well meaning and kindly.
-        <br />
-
-        {""a benevolent smile""}
-      </CoreTypographyBody2>
-    </CoreCardContent>
-
-    <CoreCardActions>
-      <CoreButton size="small">Learn More</CoreButton>
-
-    </CoreCardActions>
-  </CoreCard>
-        `}
-        renderElement={<>
-          <CoreCard styleClasses={[CoreClasses.WIDTH.MAX_W_25]}>
-            <CoreCardContent
-            >
-              <CoreTypographyBody1 color="text.secondary" gutterBottom>
-          Word of the Day
-              </CoreTypographyBody1>
-
-              <CoreTypographyBody1 variant="h5" component="div">{`be${bull}nev${bull}o${bull}lent`}</CoreTypographyBody1>
-
-              <CoreTypographyBody1 color="text.secondary">
-          adjective
-              </CoreTypographyBody1>
-
-              <CoreTypographyBody2 >
-          well meaning and kindly.
-
-                {"\"a benevolent smile\""}
-              </CoreTypographyBody2>
-            </CoreCardContent>
-
-            <CoreCardActions>
-              <CoreButton size="small">Learn More</CoreButton>
-
-            </CoreCardActions>
-          </CoreCard>
-        </>}
+                    <CoreH6 styleClasses={[CoreClasses.PADDING.PR2]}>I&apos;m also a Wrappiders</CoreH6>
+                  </CoreCardContent>
+                </CoreCard>
+              }
+            />
+          </>
+        }
       />
-        
-      {/* eslint-disable-next-line etc/no-commented-out-code */}
-      {/* <ComponentProps component={CoreCardContent} /> */}
-      
     </>
   );
 }
