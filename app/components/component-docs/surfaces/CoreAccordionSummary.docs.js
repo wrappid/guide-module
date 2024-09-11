0a2b5a2e342bf3846d19f777755400b7bf42bb36
PritamIT2023 <pritammondal8981@gmail.com>
2024-09-11 15:36:14 +0530

    docs(global): :memo: update docs CoreAccordionDetail,CoreAccordionSummary
    update docs CoreAccordionDetail,CoreAccordionSummary
    
    Ref: #302

--- app/components/component-docs/surfaces/CoreAccordionSummary.docs.js
+++ app/components/component-docs/surfaces/CoreAccordionSummary.docs.js
@@ -1,29 +1,113 @@
 import {
-  CoreH4,
+  CoreAccordion,
+  CoreAccordionDetail,
+  CoreAccordionSummary,
+  CoreIcon,
+  CorePaper,
+  CoreStack,
   CoreTypographyBody1
 } from "@wrappid/core";
   
 import CodeSample from "../../CodeSample";
+import ComponentDocs from "../ComponentDocs";
 
-export default function CoreAccordionSummaryDocs() {
+export default function CoreAccordionDetailDocs() {
   return (
     <>
-      <CoreH4>CoreAccordionSummary</CoreH4>
-  
-      <CoreTypographyBody1>
-          COMPONENT_DESCRIPTION
-      </CoreTypographyBody1>
-  
-      <CodeSample
-        title={"TITLE_OF_THE_SAMPLE (NOT_DEFINED)"}
-        description={"DESCRIPTION_OF_THE_SAMPLE"}
-        code={"PRE-FORMATTED_CODE_GOES_HERE"}
-        renderElement={<></>}
-      />
+      <ComponentDocs
+        component={CoreAccordionSummary}
+        basicSample={false}
+        description={<>
+          <CoreTypographyBody1>
+          An CoreAccordionSummary component serves as the header or title section of an accordion. It{"'"}s a foundational element within the CoreAccordion component, providing a concise and interactive way to indicate the content that will be revealed when the accordion is expanded.</CoreTypographyBody1>
+        </>}
+        samples={
+          <CodeSample
+            title={"Basic CoreAccordionSummary"}
+            description={" "}
+            code={`<CoreStack component={CorePaper}>
+  <CoreAccordion>
+    <CoreAccordionSummary expandIcon={<CoreIcon color="action" icon="arrow" />}>
+      <CoreTypographyBody1>Summary 1</CoreTypographyBody1>
+    </CoreAccordionSummary>
+    <CoreAccordionDetail>
+      <CoreTypographyBody1> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+        malesuada lacus ex, sit amet blandit leo lobortis eget.</CoreTypographyBody1>
+    </CoreAccordionDetail>
+  </CoreAccordion>
+  <CoreAccordion>
+    <CoreAccordionSummary expandIcon={<CoreIcon color="action" icon="arrow" />}>
+      <CoreTypographyBody1>Summary 2</CoreTypographyBody1>
+    </CoreAccordionSummary>
+    <CoreAccordionDetail>
+      <CoreTypographyBody1>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+        malesuada lacus ex, sit amet blandit leo lobortis eget.</CoreTypographyBody1>
+    </CoreAccordionDetail>
+  </CoreAccordion>
+</CoreStack>`}
+            expandedCode={`import {
+  CoreStack,
+  CorePaper, 
+  CoreAccordion, 
+  CoreAccordionSummary, 
+  CoreIcon, 
+  CoreTypographyBody1, 
+  CoreAccordionDetail 
+} from "@wrappid/core";
+                
+export default function BasicCoreAccordionDetail() {
+  return (  
+    <CoreStack component={CorePaper}>
+      <CoreAccordion>
+        <CoreAccordionSummary expandIcon={<CoreIcon color="action" icon="arrow" />}>
+          <CoreTypographyBody1>Summary 1</CoreTypographyBody1>
+        </CoreAccordionSummary>
+        <CoreAccordionDetail>
+          <CoreTypographyBody1> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.</CoreTypographyBody1>
+        </CoreAccordionDetail>
+      </CoreAccordion>
+      <CoreAccordion>
+        <CoreAccordionSummary expandIcon={<CoreIcon color="action" icon="arrow" />}>
+          <CoreTypographyBody1>Summary 2</CoreTypographyBody1>
+        </CoreAccordionSummary>
+        <CoreAccordionDetail>
+          <CoreTypographyBody1>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.</CoreTypographyBody1>
+        </CoreAccordionDetail>
+      </CoreAccordion>
+    </CoreStack>           
+  );              
+}`}
+            renderElement={<>
+              <CoreStack component={CorePaper}>
+                <CoreAccordion>
+                  <CoreAccordionSummary expandIcon={<CoreIcon color="action" icon="arrow" />}>
+                    <CoreTypographyBody1>Summary 1</CoreTypographyBody1>
+                  </CoreAccordionSummary>
+
+                  <CoreAccordionDetail>
+                    <CoreTypographyBody1> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.</CoreTypographyBody1>
+                  </CoreAccordionDetail>
+                </CoreAccordion>
+
+                <CoreAccordion>
+                  <CoreAccordionSummary expandIcon={<CoreIcon color="action" icon="arrow" />}>
+                    <CoreTypographyBody1>Summary 2</CoreTypographyBody1>
+                  </CoreAccordionSummary>
+
+                  <CoreAccordionDetail>
+                    <CoreTypographyBody1>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.</CoreTypographyBody1>
+                  </CoreAccordionDetail>
+                </CoreAccordion>
+              </CoreStack>    
+            </>}
+          />
         
-      {/* eslint-disable-next-line etc/no-commented-out-code */}
-      {/* <ComponentProps component={CoreAccordionSummary} /> */}
-      
+        }
+      />
     </>
   );
 }
